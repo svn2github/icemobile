8958c1dc234fc4d5e314fdc441927845c360d6fa
patrick.corless <patrick.corless@1de00972-ac04-4911-a46b-06793c8d78d9>
2011-09-13 19:11:13 +0000

    MOBI-63 added commandButtonGroup component to the button-example demo.
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@25465 1de00972-ac04-4911-a46b-06793c8d78d9

--- samples/mobileshowcase/src/main/java/org/icemobile/samples/mobileshowcase/view/examples/input/button/ButtonBean.java
+++ samples/mobileshowcase/src/main/java/org/icemobile/samples/mobileshowcase/view/examples/input/button/ButtonBean.java
@@ -22,6 +22,7 @@ import org.icemobile.samples.mobileshowcase.view.metadata.context.ExampleImpl;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.event.ActionEvent;
 import java.io.Serializable;
 
 @Destination(
@@ -56,10 +57,36 @@ public class ButtonBean extends ExampleImpl<ButtonBean> implements
 
     public static final String BEAN_NAME = "buttonBean";
 
+    private String selectedButton1  = "Yes";
+    private String selectedButton2  = "Yes";
+    private String buttonType;
+
 
     public ButtonBean() {
         super(ButtonBean.class);
     }
 
+    public void buttonType(ActionEvent event){
+        buttonType = (String)event.getComponent().getAttributes().get("buttonType");
+    }
 
+    public void commandGroup1Selected(ActionEvent event){
+        selectedButton1 = (String)event.getComponent().getAttributes().get("selectedName");
+    }
+
+    public void commandGroup2Selected(ActionEvent event){
+        selectedButton2 = (String)event.getComponent().getAttributes().get("selectedName");
+    }
+
+    public String getSelectedButton1() {
+        return selectedButton1;
+    }
+
+    public String getSelectedButton2() {
+        return selectedButton2;
+    }
+
+    public String getButtonType() {
+        return buttonType;
+    }
 }
