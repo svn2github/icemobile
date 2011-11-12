2833efbc40102cd3cc7540c3880666afec33e662
judy.guglielmin <judy.guglielmin@1de00972-ac04-4911-a46b-06793c8d78d9>
2011-11-12 01:02:02 +0000

    MOBI-79 cleanup
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@26334 1de00972-ac04-4911-a46b-06793c8d78d9

--- components/component/src/org/icefaces/component/datespinner/DateSpinnerRenderer.java
+++ components/component/src/org/icefaces/component/datespinner/DateSpinnerRenderer.java
@@ -51,7 +51,6 @@ public class DateSpinnerRenderer extends BaseInputRenderer  {
             return;
         }
         String inputValue = context.getExternalContext().getRequestParameterMap().get(clientId+"_input");
-        logger.info(" DECODING STRING="+inputValue );
         if(!isValueBlank(inputValue)) {
             dateSpinner.setSubmittedValue(inputValue);
         }
