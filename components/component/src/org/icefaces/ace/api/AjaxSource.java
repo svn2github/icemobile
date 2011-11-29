e6c6d37dcc6ab7a5d61bf6f38c2c2b6bd35e1137
judy.guglielmin <judy.guglielmin@1de00972-ac04-4911-a46b-06793c8d78d9>
2011-11-29 23:07:02 +0000

    MOBI-120 added mobi:ajax support for mobi components inputText, commandButton and flipswitch with other components to follow.  The BaseRenderers that allow mobi:ajax support now extend CoreRenderer and the base renderer support is now in it's own package, rather than in utils.  Tests are available in mobitest for the above mentioned components and the ajax tag support--forgot these support files copied directly from ace!
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@26635 1de00972-ac04-4911-a46b-06793c8d78d9

---
+++ components/component/src/org/icefaces/ace/api/AjaxSource.java
@@ -0,0 +1,51 @@
+/*
+ * Original Code developed and contributed by Prime Technology.
+ * Subsequent Code Modifications Copyright 2011 ICEsoft Technologies Canada Corp. (c)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * NOTE THIS CODE HAS BEEN MODIFIED FROM ORIGINAL FORM
+ *
+ * Subsequent Code Modifications have been made and contributed by ICEsoft Technologies Canada Corp. (c).
+ *
+ * Code Modification 1: Integrated with ICEfaces Advanced Component Environment.
+ * Contributors: ICEsoft Technologies Canada Corp. (c)
+ *
+ * Code Modification 2: [ADD BRIEF DESCRIPTION HERE]
+ * Contributors: ______________________
+ * Contributors: ______________________
+ */
+package org.icefaces.ace.api;
+
+/**
+ * AjaxSource is the contract that needs to be implemented by components that fully implement all 
+ * configuration options of PPR
+ */
+public interface AjaxSource {
+
+	public String getOnstart();
+	
+	public String getOncomplete();
+	
+	public String getOnsuccess();
+	
+	public String getOnerror();
+	
+	public String getUpdate();
+	
+	public String getProcess();
+	
+/*	public boolean isGlobal();
+	
+	public boolean isAsync(); */
+}
