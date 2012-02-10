cfacab99feab7851b8533edf985df40f72cb1c92
patrick.corless <patrick.corless@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-02-10 14:11:45 +0000

    MOBI-173 updates to licensing headers for Apache 2.0.
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@27751 1de00972-ac04-4911-a46b-06793c8d78d9

--- components/component/src/org/icefaces/ace/facelets/MethodRule.java
+++ components/component/src/org/icefaces/ace/facelets/MethodRule.java
@@ -1,29 +1,17 @@
 /*
- * Original Code developed and contributed by Prime Technology.
- * Subsequent Code Modifications Copyright 2011 ICEsoft Technologies Canada Corp. (c)
+ * Copyright 2004-2012 ICEsoft Technologies Canada Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * NOTE THIS CODE HAS BEEN MODIFIED FROM ORIGINAL FORM
- *
- * Subsequent Code Modifications have been made and contributed by ICEsoft Technologies Canada Corp. (c).
- *
- * Code Modification 1: Integrated with ICEfaces Advanced Component Environment.
- * Contributors: ICEsoft Technologies Canada Corp. (c)
- *
- * Code Modification 2: [ADD BRIEF DESCRIPTION HERE]
- * Contributors: ______________________
- * Contributors: ______________________
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
  */
 package org.icefaces.ace.facelets;
 
