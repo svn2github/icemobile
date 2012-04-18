8c2cf5c88a177e309bb7265e5ecf4f4c8cb9eea0
greg.dick <greg.dick@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-04-18 22:45:42 +0000

    MOBI-217 enable non-default form action parameters for Spring-mvc applications
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@28753 1de00972-ac04-4911-a46b-06793c8d78d9

--- client/blackberry/container/src/org/icemobile/client/blackberry/http/HttpRequestHandler.java
+++ client/blackberry/container/src/org/icemobile/client/blackberry/http/HttpRequestHandler.java
@@ -12,7 +12,7 @@
  * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  * express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
- */  
+ */
 package org.icemobile.client.blackberry.http;
 
 import java.io.ByteArrayOutputStream;
@@ -28,68 +28,68 @@ import net.rim.device.api.io.http.HttpHeaders;
 
 public class HttpRequestHandler {
 
-    private ContainerController mController; 
+    private ContainerController mController;
 
-    public HttpRequestHandler (ContainerController controller) { 
+    public HttpRequestHandler(ContainerController controller) {
         mController = controller;
     }
 
     /**
-     * Form up some details and forward a request to the server. 
-     * The  original BrowserField) must 
-     * make the request (rather than our doing so using HttpConnection 
-     * directly) in order to maintain the JSessionId cookies during 
-     * upload and to allow the BrowserField to properly utilize the 
-     * configured Network transport infrastructure on the device. 
-     * 
-     * @param authToken An optional authentication token
-     * @param requestData A MultipartPostData object containing a series of 
-     *        Parts for the upload
-     * @return returns the result string read from the input stream 
+     * Form up some details and forward a request to the server.
+     * The  original BrowserField) must
+     * make the request (rather than our doing so using HttpConnection
+     * directly) in order to maintain the JSessionId cookies during
+     * upload and to allow the BrowserField to properly utilize the
+     * configured Network transport infrastructure on the device.
+     *
+     * @param authToken   An optional authentication token
+     * @param requestData A MultipartPostData object containing a series of
+     *                    Parts for the upload
+     * @return returns the result string read from the input stream
      */
-    public String processAJAXRequest(String authToken, 
-            MultipartPostData request) { 
+    public String processAJAXRequest(String actionMethod, String authToken,
+                                     MultipartPostData request) {
 
-        String type = request.getContentType();		
+        String type = request.getContentType();
 
         try {
 
             HttpHeaders headers = new HttpHeaders();
-            headers.addProperty("Content-Type", type );
-            headers.addProperty("Connection", " close" );
-            headers.addProperty("Faces-Request", "partial/ajax" );
+            headers.addProperty("Content-Type", type);
+            headers.addProperty("Connection", " close");
+            headers.addProperty("Faces-Request", "partial/ajax");
             //			
-            if (authToken != null && authToken.length()>0) { 
+            if (authToken != null && authToken.length() > 0) {
 
-                byte[] ByteArray=authToken.getBytes("UTF-8");
-                String Encoded = 
-                    Base64OutputStream.encodeAsString(
-                            ByteArray,
-                            0, 
-                            ByteArray.length, 
-                            false, 
-                            false);
-                headers.addProperty("Authorization",Encoded);
+                byte[] ByteArray = authToken.getBytes("UTF-8");
+                String Encoded =
+                        Base64OutputStream.encodeAsString(
+                                                                 ByteArray,
+                                                                 0,
+                                                                 ByteArray.length,
+                                                                 false,
+                                                                 false);
+                headers.addProperty("Authorization", Encoded);
             }
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();	
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
             request.write(bos);
 
             bos.flush();
             bos.close();
 
             // This doesn't lend itself very well to the progress meter. 
-            InputConnection ic = 
-                mController.postRequest( bos.toString(), headers);
+            InputConnection ic =
+                    mController.postRequest(actionMethod, bos.toString(), headers);
             InputStream inputStream = ic.openInputStream();
             StringBuffer sb = new StringBuffer();
 
             int C;
-            while( -1 != (C = inputStream.read())) { 
-                sb.append((char)C);
+            while (-1 != (C = inputStream.read())) {
+                sb.append((char) C);
             }
             return sb.toString();
         } catch (Exception e) {
-        	Logger.ERROR("Exception in HTTPRequest processing: " + e.getMessage());
+            Logger.ERROR("Exception in HTTPRequest processing: " + e.getMessage());
         }
         return null;
     }
