48372672ad4ea4e7317fbb94b99560487c91e148
greg.dick <greg.dick@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-04-18 22:46:30 +0000

    MOBI-217 enable non-default form action parameters for Spring-mvc applications
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@28755 1de00972-ac04-4911-a46b-06793c8d78d9

--- client/blackberry/container/src/org/icemobile/client/blackberry/script/upload/AjaxUpload.java
+++ client/blackberry/container/src/org/icemobile/client/blackberry/script/upload/AjaxUpload.java
@@ -12,7 +12,7 @@
  * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  * express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
- */ 
+ */
 package org.icemobile.client.blackberry.script.upload;
 
 import java.io.ByteArrayOutputStream;
@@ -34,46 +34,48 @@ import net.rim.device.api.script.ScriptableFunction;
 import net.rim.device.api.ui.UiApplication;
 
 /**
- * A fileUpload javascript accessable extension that is AJAX aware. 
- *
+ * A fileUpload javascript accessable extension that is AJAX aware.
  */
 public class AjaxUpload extends ScriptableFunction {
 
     private ContainerController mController;
-    private HttpRequestHandler mHandler; 
+    private HttpRequestHandler mHandler;
     private static int uploadIndex;
     private String START_OF_CDATA = "<![CDATA[";
 
-    public AjaxUpload( ContainerController controller ) { 
+    public AjaxUpload(ContainerController controller) {
 
-        mController = controller; 
+        mController = controller;
         mHandler = new HttpRequestHandler(controller);
     }
 
-    public Object invoke( Object thiz, Object[] args) { 
+    public Object invoke(Object thiz, Object[] args) {
 
-        if (args.length != 1) { 
-        	Logger.ERROR( "ajax.upload - wrong number of arguments:" + args.length);
-            return Boolean.FALSE; 
+        if (args.length != 2) {
+            Logger.ERROR("ajax.upload - wrong number of arguments:" + args.length);
+            return Boolean.FALSE;
         }
-        String serializedForm = (String) args[0];
+
+        final String actionMethod = (String) args[0];
+        String serializedForm = (String) args[1];
+        Logger.DEBUG("ajax.upload - actionMethod: " + actionMethod);
 
         //boolean gotValue = true;
         //String[] result;
         //		MultipartEntity content = new MultipartEntity();
         final MultipartPostData content = new MultipartPostData();
         final long startTime = System.currentTimeMillis();
-        
+
         try {
 //            Logger.DEBUG("ajax.upload - Serialized form:\n" + serializedForm);
             NameValuePair[] params = UploadUtilities.getNameValuePairs(serializedForm, "=", "&");
             //Log.e("ICEutil", "NVP=" + params.length);
-            for (int i=0; i<params.length; i++) {
+            for (int i = 0; i < params.length; i++) {
                 if (params[i].getName().indexOf("file") > -1) {
                     String filename = HttpUtils.URLdecode(params[i].getValue());
                     Logger.DEBUG("ajax.upload - File part encountered: " + filename);
 
-                    if (filename.indexOf(".jpg")> -1 ) { 
+                    if (filename.indexOf(".jpg") > -1) {
                         byte[] imageBytes;
                         InputStream theImage;
                         FileConnection fconn = (FileConnection) Connector.open(filename);
@@ -81,17 +83,17 @@ public class AjaxUpload extends ScriptableFunction {
                         imageBytes = new byte[(int) fconn.fileSize()];
                         theImage = fconn.openInputStream();
                         theImage.read(imageBytes);
-                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream( imageBytes.length );
-                        byteArrayOutputStream.write( imageBytes );
+                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(imageBytes.length);
+                        byteArrayOutputStream.write(imageBytes);
 
                         byteArrayOutputStream.flush();
-                        byteArrayOutputStream.close();		
+                        byteArrayOutputStream.close();
 
-                        String name = HttpUtils.URLdecode(params[i].getName()); 
-                        String imageVar = byteArrayOutputStream.toString(); 
+                        String name = HttpUtils.URLdecode(params[i].getName());
+                        String imageVar = byteArrayOutputStream.toString();
                         content.appendImagePart(name,
-                                filename, 
-                                imageVar );
+                                                       filename,
+                                                       imageVar);
                     } else if (filename.indexOf(".amr") > -1) {
 
                         byte[] clipBytes;
@@ -103,18 +105,18 @@ public class AjaxUpload extends ScriptableFunction {
                         clipBytes = new byte[(int) fconn.fileSize()];
                         theClip = fconn.openInputStream();
                         theClip.read(clipBytes);
-                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream( clipBytes.length );
-                        byteArrayOutputStream.write( clipBytes );
+                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(clipBytes.length);
+                        byteArrayOutputStream.write(clipBytes);
 
                         byteArrayOutputStream.flush();
-                        byteArrayOutputStream.close();		
+                        byteArrayOutputStream.close();
 
-                        String name = HttpUtils.URLdecode(params[i].getName()); 
-                        String audioVar = byteArrayOutputStream.toString(); 
+                        String name = HttpUtils.URLdecode(params[i].getName());
+                        String audioVar = byteArrayOutputStream.toString();
                         content.appendAudioPart(name,
-                                filename, 
-                                audioVar );
-                    } else if (filename.indexOf(".3gp") > -1) { 
+                                                       filename,
+                                                       audioVar);
+                    } else if (filename.indexOf(".3gp") > -1) {
                         byte[] videoBytes;
                         InputStream theVideo;
                         FileConnection fconn = (FileConnection) Connector.open(filename);
@@ -122,86 +124,86 @@ public class AjaxUpload extends ScriptableFunction {
                         videoBytes = new byte[(int) fconn.fileSize()];
                         theVideo = fconn.openInputStream();
                         theVideo.read(videoBytes);
-                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream( videoBytes.length );
-                        byteArrayOutputStream.write( videoBytes );
+                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(videoBytes.length);
+                        byteArrayOutputStream.write(videoBytes);
 
                         byteArrayOutputStream.flush();
-                        byteArrayOutputStream.close();		
+                        byteArrayOutputStream.close();
 
-                        String name = HttpUtils.URLdecode(params[i].getName()); 
-                        String videoVar = byteArrayOutputStream.toString(); 
+                        String name = HttpUtils.URLdecode(params[i].getName());
+                        String videoVar = byteArrayOutputStream.toString();
                         content.appendVideoPart(name,
-                                filename, 
-                                videoVar );
+                                                       filename,
+                                                       videoVar);
                     }
 
                 } else {
-                    if (params[i].getName() == null || params[i].getName().equals("")) { 
+                    if (params[i].getName() == null || params[i].getName().equals("")) {
                         continue;
                     }
-                    content.append( HttpUtils.URLdecode(params[i].getName()), HttpUtils.URLdecode(params[i].getValue()));
+                    content.append(HttpUtils.URLdecode(params[i].getName()), HttpUtils.URLdecode(params[i].getValue()));
                 }
             }
 
-           
-            final DisposableResultHolder drh = new DisposableResultHolder();
-            UiApplication.getUiApplication().invokeLater( new Runnable() { 
-                public void run() { 
 
-                    String response = mHandler.processAJAXRequest( null, content );
-               
+            final DisposableResultHolder drh = new DisposableResultHolder();
+            UiApplication.getUiApplication().invokeLater(new Runnable() {
+                public void run() {
+
+                    String response = mHandler.processAJAXRequest(actionMethod, null, content);
+
                     Logger.TIME(startTime, "ajax.upload FormUpload");
                     Logger.DEBUG("ajax.upload - Raw response: " + response);
-               
-                    if (response.indexOf( "<error>") > -1) {
-                    	processError(response); 
-                    } else { 
-            			drh.setResult(response);
-            			if (response != null) { 
-            				mController.processResult( "AjaxUpload_" + uploadIndex++,  drh);
-            			}
+
+                    if (response.indexOf("<error>") > -1) {
+                        processError(response);
+                    } else {
+                        drh.setResult(response);
+                        if (response != null) {
+                            mController.processResult("AjaxUpload_" + uploadIndex++, drh);
+                        }
                     }
                 }
             });
 
 
-        } catch (Exception e) { 
-        	Logger.ERROR ("ajax.upload - Upload exception: " + e);
+        } catch (Exception e) {
+            Logger.ERROR("ajax.upload - Upload exception: " + e);
             return Boolean.FALSE;
         }
         return Boolean.TRUE;
     }
 
-    private void processError(String response) { 
-    	
-    	int spos = response.indexOf( START_OF_CDATA );
-    	String error = null; 
-        if (spos > -1) { 
-        	spos += START_OF_CDATA.length(); 
-        	int epos  = response.indexOf("]]>", spos + 1 ); 
-        	if (epos > -1) { 
-        		error = response.substring(spos, epos); 
-        		Logger.DIALOG("Error in upload: " + error); 
-        	}
-        } else { 
-        	Logger.DIALOG("Error in upload... check container log for message contents");
-        }    	
-    }
-   
-    class DisposableResultHolder implements ResultHolder { 
+    private void processError(String response) {
 
-        private String result; 
-        
-        public void setResult(String result) { 
-            if (result != null) { 
-                this.result = new String (result); 
-            } else { 
-            	Logger.ERROR("Null result in ajaxUpload" );
+        int spos = response.indexOf(START_OF_CDATA);
+        String error = null;
+        if (spos > -1) {
+            spos += START_OF_CDATA.length();
+            int epos = response.indexOf("]]>", spos + 1);
+            if (epos > -1) {
+                error = response.substring(spos, epos);
+                Logger.DIALOG("Error in upload: " + error);
+            }
+        } else {
+            Logger.DIALOG("Error in upload... check container log for message contents");
+        }
+    }
+
+    class DisposableResultHolder implements ResultHolder {
+
+        private String result;
+
+        public void setResult(String result) {
+            if (result != null) {
+                this.result = new String(result);
+            } else {
+                Logger.ERROR("Null result in ajaxUpload");
             }
         }
-        
-        public String getResult() { 
-            return this.result; 
+
+        public String getResult() {
+            return this.result;
         }
     }
 
