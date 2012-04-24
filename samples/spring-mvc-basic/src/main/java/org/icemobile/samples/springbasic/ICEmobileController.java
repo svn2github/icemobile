af964019f621f8cf5532ea6811fb466b51942a8c
ted.goddard <ted.goddard@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-04-24 19:49:19 +0000

    HTML/json-based camera and microphone (MOBI-217)
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@28801 1de00972-ac04-4911-a46b-06793c8d78d9

--- samples/spring-mvc-basic/src/main/java/org/icemobile/samples/springbasic/ICEmobileController.java
+++ samples/spring-mvc-basic/src/main/java/org/icemobile/samples/springbasic/ICEmobileController.java
@@ -1,7 +1,5 @@
 package org.icemobile.samples.springbasic;
 
-import java.io.IOException;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,14 +7,16 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-@RequestMapping("/icemobile")
 @SessionAttributes("icemobileBean")
 public class ICEmobileController {
 
@@ -50,4 +50,42 @@ public class ICEmobileController {
         
 	}
 	
+	@RequestMapping(value = "/jsoncam", method=RequestMethod.POST)
+	public @ResponseBody CamUpdate jsonCamera(HttpServletRequest request, ModelBean modelBean, @RequestParam(value = "camera-file", required = false) MultipartFile file, @RequestParam(value = "camera", required = false) MultipartFile inputFile, Model model) throws IOException {
+
+        String fileName = "empty";
+        String uuid = Long.toString(
+                Math.abs(UUID.randomUUID().getMostSignificantBits()), 32 );
+        String newFileName = "/resources/img-" + uuid + ".jpg";
+        if (null != file)  {
+            fileName = file.getOriginalFilename();
+            file.transferTo(new File(request.getRealPath(newFileName)));
+        }
+        if (null != inputFile)  {
+            fileName = inputFile.getOriginalFilename();
+            inputFile.transferTo(new File(request.getRealPath(newFileName)));
+        }
+
+        return new CamUpdate("Thanks for the photo, " + modelBean.getName(), 
+                request.getContextPath() + newFileName);
+    }
+
+}
+
+class CamUpdate  {
+    private String path;
+    private String message;
+
+    public CamUpdate(String message, String path)  {
+        this.path = path;
+        this.message = message;
+    }
+
+    public String getPath()  {
+        return path;
+    }
+
+    public String getMessage()  {
+        return message;
+    }
 }
--- samples/spring-mvc-basic/src/main/java/org/icemobile/samples/springbasic/MicrophoneController.java
+++ samples/spring-mvc-basic/src/main/java/org/icemobile/samples/springbasic/MicrophoneController.java
@@ -1,7 +1,5 @@
 package org.icemobile.samples.springbasic;
 
-import java.io.IOException;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,10 +7,13 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -43,7 +44,7 @@ public class MicrophoneController {
     }
 
 	@RequestMapping(value = "/microphone", method=RequestMethod.POST)
-	public void processMicrophone(HttpServletRequest request, ModelBean modelBean, @RequestParam(value = "mic-file", required = false) MultipartFile file, @RequestParam(value = "camera", required = false) MultipartFile inputFile, Model model) throws IOException {
+	public void processMicrophone(HttpServletRequest request, ModelBean modelBean, @RequestParam(value = "mic-file", required = false) MultipartFile file, @RequestParam(value = "mic", required = false) MultipartFile inputFile, Model model) throws IOException {
 
         String fileName = "empty";
         if (null != file)  {
@@ -58,4 +59,42 @@ public class MicrophoneController {
 
     }
 
+	@RequestMapping(value = "/jsonmic", method=RequestMethod.POST)
+	public @ResponseBody MicUpdate jsonMicrophone(HttpServletRequest request, ModelBean modelBean, @RequestParam(value = "mic-file", required = false) MultipartFile file, @RequestParam(value = "mic", required = false) MultipartFile inputFile, Model model) throws IOException {
+
+        String fileName = "empty";
+        String uuid = Long.toString(
+                Math.abs(UUID.randomUUID().getMostSignificantBits()), 32 );
+        String newFileName = "/media/clip-" + uuid + ".mp4";
+        if (null != file)  {
+            fileName = file.getOriginalFilename();
+            file.transferTo(new File(request.getRealPath(newFileName)));
+        }
+        if (null != inputFile)  {
+            fileName = inputFile.getOriginalFilename();
+            inputFile.transferTo(new File(request.getRealPath(newFileName)));
+        }
+
+        return new MicUpdate("Thanks for the sound, " + modelBean.getName(), 
+                request.getContextPath() + newFileName);
+    }
+
+}
+
+class MicUpdate  {
+    private String clipLocation;
+    private String message;
+
+    public MicUpdate( String message, String clipLocation)  {
+        this.clipLocation = clipLocation;
+        this.message = message;
+    }
+
+    public String getClipLocation()  {
+        return clipLocation;
+    }
+
+    public String getMessage()  {
+        return message;
+    }
 }
