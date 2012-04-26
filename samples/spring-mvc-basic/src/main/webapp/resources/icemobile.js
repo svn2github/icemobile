dff0cf94b1e0f62bdaad1cb1243647f83fdaf233
ted.goddard <ted.goddard@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-04-26 17:19:37 +0000

    prototype ICEmobile-SX HTML integration via onhashchange (MOBI-217)
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@28831 1de00972-ac04-4911-a46b-06793c8d78d9

---
+++ samples/spring-mvc-basic/src/main/webapp/resources/icemobile.js
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2004-2012 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+
+if (!window['ice']) {
+    window.ice = {};
+}
+
+ice.registerAuxUpload = function (sessionid, uploadURL) {
+    var auxiframe = document.getElementById('auxiframe');
+    if (null == auxiframe) {
+        auxiframe = document.createElement('iframe');
+        auxiframe.setAttribute("id", "auxiframe");
+        auxiframe.setAttribute("style", "width:0px; height:0px; border: 0px");
+        auxiframe.setAttribute("src",
+                "icemobile://c=register&r=" +
+                        escape(window.location) + "&JSESSIONID=" + sessionid +
+                        "&u=" + escape(uploadURL)
+        );
+        document.body.appendChild(auxiframe);
+    }
+};
+
+ice.mobilesx = function (element) {
+    var formAction = ice.formOf(element).getAttribute("action");
+    var command = element.getAttribute("data-command");
+    var id = element.getAttribute("id");
+    var uploadURL;
+    if (0 === formAction.indexOf("/"))  {
+        uploadURL = window.location.origin + formAction;
+    } else if ((0 === formAction.indexOf("http://")) || 
+        (0 === formAction.indexOf("https://"))) {
+        uploadURL = formAction;
+    } else {
+        var baseURL = window.location.toString();
+        uploadURL = baseURL.substring(0, baseURL.lastIndexOf("/")) +
+            "/" + formAction;
+    }
+    var returnURL = window.location + "#icemobilesx";
+
+    var sxURL = "icemobile://c=" + escape(command + "?id=" + id) +
+        "&u=" + escape(uploadURL) + "&r=" + escape(returnURL);
+        
+    window.location = sxURL;
+}
+
+ice.formOf = function(element)  {
+    var parent = element;
+    while (null != parent)  {
+        if ("form" == parent.nodeName.toLowerCase())  {
+            return parent;
+        }
+        parent = parent.parentNode;
+    }
+}
