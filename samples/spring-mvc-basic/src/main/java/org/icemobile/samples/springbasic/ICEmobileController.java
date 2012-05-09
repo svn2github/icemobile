7daae93789f6ff8104dc5cb37ee67066d0f616e6
greg.dick <greg.dick@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-05-09 21:41:32 +0000

    MOBI-217 Added handlers for the 'various' UI page
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@28983 1de00972-ac04-4911-a46b-06793c8d78d9

--- samples/spring-mvc-basic/src/main/java/org/icemobile/samples/springbasic/ICEmobileController.java
+++ samples/spring-mvc-basic/src/main/java/org/icemobile/samples/springbasic/ICEmobileController.java
@@ -17,140 +17,166 @@ import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
+import javax.xml.ws.http.HTTPException;
 
 @Controller
 public class ICEmobileController {
 
-	@ModelAttribute
-	public void ajaxAttribute(WebRequest request, Model model) {
-		model.addAttribute("ajaxRequest", AjaxUtils.isAjaxRequest(request));
-	}
+    @ModelAttribute
+    public void ajaxAttribute(WebRequest request, Model model) {
+        model.addAttribute("ajaxRequest", AjaxUtils.isAjaxRequest(request));
+    }
 
-	@RequestMapping(value="/icemobile", method=RequestMethod.GET)
-	public void fileUploadForm() {
-	}
+    @RequestMapping(value = "/icemobile", method = RequestMethod.GET)
+    public void fileUploadForm() {
+    }
 
-	@RequestMapping(value="/camregion", method=RequestMethod.GET)
-	public void camRegion(HttpServletRequest request, Model model) {
-		model.addAttribute("isGET", Boolean.TRUE);
-		model.addAttribute("imgPath", getCurrentFileName(request));
-	}
+    @RequestMapping(value = "/camregion", method = RequestMethod.GET)
+    public void camRegion(HttpServletRequest request, Model model) {
+        model.addAttribute("isGET", Boolean.TRUE);
+        model.addAttribute("imgPath", getCurrentFileName(request));
+    }
 
-	@RequestMapping(value="/campush", method=RequestMethod.GET)
-	public void camPush(HttpServletRequest request, Model model) {
-		model.addAttribute("isGET", Boolean.TRUE);
-		model.addAttribute("imgPath", getCurrentFileName(request));
-	}
+    @RequestMapping(value = "/campush", method = RequestMethod.GET)
+    public void camPush(HttpServletRequest request, Model model) {
+        model.addAttribute("isGET", Boolean.TRUE);
+        model.addAttribute("imgPath", getCurrentFileName(request));
+    }
 
-	@RequestMapping(value="/campushr", method=RequestMethod.GET)
-	public void camPushr(HttpServletRequest request, Model model) {
-		model.addAttribute("isGET", Boolean.TRUE);
-		model.addAttribute("imgPath", getCurrentFileName(request));
-	}
+    @RequestMapping(value = "/campushr", method = RequestMethod.GET)
+    public void camPushr(HttpServletRequest request, Model model) {
+        model.addAttribute("isGET", Boolean.TRUE);
+        model.addAttribute("imgPath", getCurrentFileName(request));
+    }
 
-	@ModelAttribute("icemobileBean")
-	public ModelBean createBean() {
-		return new ModelBean();
-	}
+    @ModelAttribute("icemobileBean")
+    public ModelBean createBean() {
+        return new ModelBean();
+    }
 
-	@RequestMapping(value="/icemobile", method=RequestMethod.POST)
-	public void processUpload(HttpServletRequest request, ModelBean modelBean, @RequestParam(value = "camera-file", required = false) MultipartFile file, @RequestParam(value = "camera", required = false) MultipartFile inputFile, Model model) throws IOException {
+    @RequestMapping(value = "/icemobile", method = RequestMethod.POST)
+    public void processUpload(HttpServletRequest request, ModelBean modelBean,
+                              @RequestParam(value = "camera-file", required = false) MultipartFile file,
+                              @RequestParam(value = "camera", required = false) MultipartFile inputFile,
+                              Model model) throws IOException {
         String newFileName = saveImage(request, file, inputFile);
-        if (((null != file) && !file.isEmpty()) || ((null != inputFile) && !inputFile.isEmpty()))  {
+        if (((null != file) && !file.isEmpty()) || ((null != inputFile) && !inputFile.isEmpty())) {
             model.addAttribute("message", "Hello " + modelBean.getName() + ", your file '" + newFileName + "' was uploaded successfully.");
         }
-        if (null != newFileName)  {
+        if (null != newFileName) {
             model.addAttribute("imgPath", newFileName);
         } else {
             model.addAttribute("imgPath", "resources/uploaded.jpg");
         }
-	}
+    }
 
-	@RequestMapping(value = "/campushr", method=RequestMethod.POST)
-	public void pushCamerar(HttpServletRequest request, ModelBean modelBean, @RequestParam(value = "camera-file", required = false) MultipartFile file, @RequestParam(value = "camera", required = false) MultipartFile inputFile, Model model) throws IOException {
-        
+    @RequestMapping(value = "/campushr", method = RequestMethod.POST)
+    public void pushCamerar(HttpServletRequest request, ModelBean modelBean,
+                            @RequestParam(value = "camera-file", required = false) MultipartFile file,
+                            @RequestParam(value = "camera", required = false) MultipartFile inputFile,
+                            Model model) throws IOException {
+
         this.pushCamera(request, modelBean, file, inputFile, model);
     }
 
-	@RequestMapping(value = "/campush", method=RequestMethod.POST)
-	public void pushCamera(HttpServletRequest request, ModelBean modelBean, @RequestParam(value = "camera-file", required = false) MultipartFile file, @RequestParam(value = "camera", required = false) MultipartFile inputFile, Model model) throws IOException {
+    @RequestMapping(value = "/campush", method = RequestMethod.POST)
+    public void pushCamera(HttpServletRequest request, ModelBean modelBean,
+                           @RequestParam(value = "camera-file", required = false) MultipartFile file,
+                           @RequestParam(value = "camera", required = false) MultipartFile inputFile,
+                           Model model) throws IOException {
 
         String newFileName = saveImage(request, file, inputFile);
-        if (((null != file) && !file.isEmpty()) || ((null != inputFile) && !inputFile.isEmpty()))  {
+        if (((null != file) && !file.isEmpty()) || ((null != inputFile) && !inputFile.isEmpty())) {
             model.addAttribute("message", "Hello " + modelBean.getName() + ", your file '" + newFileName + "' was uploaded successfully.");
             PushContext.getInstance(request.getServletContext())
                     .push("camPush");
         }
-        if (null != newFileName)  {
+        if (null != newFileName) {
             model.addAttribute("imgPath", newFileName);
         } else {
             model.addAttribute("imgPath", "resources/uploaded.jpg");
         }
     }
 
-	@RequestMapping(value = "/jsoncam", method=RequestMethod.POST)
-	public @ResponseBody CamUpdate jsonCamera(HttpServletRequest request, ModelBean modelBean, @RequestParam(value = "camera-file", required = false) MultipartFile file, @RequestParam(value = "camera", required = false) MultipartFile inputFile, Model model) throws IOException {
+    @RequestMapping(value = "/various", method = RequestMethod.GET)
+    public String variousGet(HttpServletRequest request, Model model) {
+        return "various";
+    }
+
+    @RequestMapping(value = "/various", method = RequestMethod.POST)
+    public String variousPost(HttpServletRequest request, Model model) {
+        return "various";
+    }
+
+    @RequestMapping(value = "/jsoncam", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    CamUpdate jsonCamera(HttpServletRequest request, ModelBean modelBean,
+                         @RequestParam(value = "camera-file", required = false) MultipartFile file,
+                         @RequestParam(value = "camera", required = false) MultipartFile inputFile,
+                         Model model) throws IOException {
 
         String newFileName = saveImage(request, file, inputFile);
 
-        return new CamUpdate("Thanks for the photo, " + modelBean.getName(), 
-                request.getContextPath() + "/" + newFileName);
+        return new CamUpdate("Thanks for the photo, " + modelBean.getName(),
+                             request.getContextPath() + "/" + newFileName);
     }
 
-    private String saveImage(HttpServletRequest request, 
-            MultipartFile file, MultipartFile inputFile) throws IOException {
+    private String saveImage(HttpServletRequest request,
+                             MultipartFile file, MultipartFile inputFile) throws
+            IOException {
 
         String fileName = null;
         String uuid = Long.toString(
-                Math.abs(UUID.randomUUID().getMostSignificantBits()), 32 );
+                                           Math.abs(UUID.randomUUID().getMostSignificantBits()), 32);
         String newFileName = "resources/img-" + uuid + ".jpg";
-        if ((null != file) && !file.isEmpty())  {
+        if ((null != file) && !file.isEmpty()) {
             fileName = file.getOriginalFilename();
             file.transferTo(new File(request.getRealPath("/" + newFileName)));
             request.getServletContext().setAttribute(
-                    this.getClass().getName(), newFileName );
+                                                            this.getClass().getName(), newFileName);
         }
-        if ((null != inputFile) && !inputFile.isEmpty())  {
+        if ((null != inputFile) && !inputFile.isEmpty()) {
             fileName = inputFile.getOriginalFilename();
             inputFile.transferTo(
-                    new File(request.getRealPath("/" + newFileName)) );
+                                        new File(request.getRealPath("/" + newFileName)));
             request.getServletContext().setAttribute(
-                    this.getClass().getName(), newFileName );
+                                                            this.getClass().getName(), newFileName);
         }
 
-        if (null == fileName)  {
+        if (null == fileName) {
             //use previously uploaded file, such as from ICEmobile-SX
             newFileName = getCurrentFileName(request);
 
         }
-        
+
         return newFileName;
     }
 
-    private String getCurrentFileName(HttpServletRequest request)  {
+    private String getCurrentFileName(HttpServletRequest request) {
         String currentName = (String) request.getServletContext().getAttribute(
-                    this.getClass().getName() );
-        if (null == currentName)  {
+                                                                                      this.getClass().getName());
+        if (null == currentName) {
             return "resources/uploaded.jpg";
         }
         return currentName;
     }
 }
 
-class CamUpdate  {
+class CamUpdate {
     private String path;
     private String message;
 
-    public CamUpdate(String message, String path)  {
+    public CamUpdate(String message, String path) {
         this.path = path;
         this.message = message;
     }
 
-    public String getPath()  {
+    public String getPath() {
         return path;
     }
 
-    public String getMessage()  {
+    public String getMessage() {
         return message;
     }
 }
