f6d1d6653fe37cb92eac38b6415bfcc276f79caf
greg.dick <greg.dick@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-06-05 17:43:17 +0000

    MOBI-231 Changed AudioRecorder to Screen implementation
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@29306 1de00972-ac04-4911-a46b-06793c8d78d9

--- client/blackberry/container/src/org/icemobile/client/blackberry/script/audio/AudioRecorder.java
+++ client/blackberry/container/src/org/icemobile/client/blackberry/script/audio/AudioRecorder.java
@@ -12,193 +12,335 @@
  * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  * express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
- */ 
+ */
 package org.icemobile.client.blackberry.script.audio;
 
-import java.util.Hashtable;
+import java.io.IOException;
 
+import javax.microedition.io.Connector;
+import javax.microedition.io.file.FileConnection;
 import javax.microedition.media.Player;
 import javax.microedition.media.control.RecordControl;
 
 import org.icemobile.client.blackberry.ContainerController;
 import org.icemobile.client.blackberry.Logger;
 import org.icemobile.client.blackberry.utils.FileUtils;
-import org.icemobile.client.blackberry.utils.NameValuePair;
-import org.icemobile.client.blackberry.utils.UploadUtilities;
 
-import net.rim.device.api.script.ScriptableFunction;
+import net.rim.device.api.ui.Field;
+import net.rim.device.api.ui.FieldChangeListener;
+import net.rim.device.api.ui.UiApplication;
+import net.rim.device.api.ui.component.ButtonField;
+import net.rim.device.api.ui.component.TextField;
+import net.rim.device.api.ui.container.HorizontalFieldManager;
+import net.rim.device.api.ui.container.MainScreen;
 
-public class AudioRecorder extends ScriptableFunction {
+/**
+ * Implement the AudioRecorder now as a Screen Implementation
+ * With GUI controls to drive the previously implicit state
+ * behaviour
+ */
+public class AudioRecorder extends MainScreen {
 
     private ContainerController mController;
     private AudioRecorderThread mRecorderThread;
 
     private String mCurrentFilename;
-    private String mFieldId; 
+    private String mFieldId;
 
     private int mMaxDuration;
     private Thread mTerminatorThread;
 
-    // no enum construct
-    private int mDeviceState; 
-    private final int idle = 0; 
-    private final int starting = 1; 
-    private final int recording = 2; 
-    private final int stopping = 3; 
+    // no enum construct. The recordingComplete state is essentially 
+    // idle, but with having a clip recorded which must be tidied up if 
+    // the user moves to record another without saving. 
+    private int mDeviceState;
+    private final int idle = 0;
+    private final int starting = 1;
+    private final int recording = 2;
+    private final int stopping = 3;
+    private final int recordingComplete = 4;
 
     private Object lockObject = new Object();
 
 
+    private ButtonField mStartStopButton;
+    private ButtonField mOkButton;
+    private ButtonField mCancelButton;
 
-    public AudioRecorder (ContainerController controller) {
+    private TextField mStatusField;
+
+    public AudioRecorder(ContainerController controller) {
         mController = controller;
+        initGui();
     }
 
-    /** 
-     * Use this if things get confused to reset the audio state
+    /**
+     * Initialize the GUI for
      */
-    public void resetAudioState() { 
+    private void initGui() {
 
-        if (mRecorderThread != null) { 
+        mStartStopButton = new ButtonField("Start Recording...", Field.FIELD_HCENTER | Field.FIELD_TOP);
+
+        mStartStopButton.setChangeListener(new FieldChangeListener() {
+            public void fieldChanged(Field field, int context) {
+
+                switch (mDeviceState) {
+                    case idle:
+                        startRecording();
+                        break;
+                    case recording:
+                        haltRecording();
+                        break;
+                    case starting:
+                        Logger.DIALOG("Audio recording starting");
+                        break;
+                    case stopping:
+                        Logger.DIALOG("Audio recording stopping...");
+                        break;
+                    case recordingComplete:
+
+                }
+            }
+        });
+
+        add(mStartStopButton);
+        mStatusField = new TextField(Field.FIELD_HCENTER | Field.FIELD_VCENTER);
+        add(mStatusField);
+
+        HorizontalFieldManager horizontalManager = new HorizontalFieldManager(Field.FIELD_BOTTOM);
+        horizontalManager.setMargin(100, 10, 0, 10);
+        add(horizontalManager);
+
+        mOkButton = new ButtonField("Ok", Field.FIELD_LEFT);
+        mOkButton.setMargin(4, 50, 4, 20);
+        mOkButton.setChangeListener(new FieldChangeListener() {
+
+            public void fieldChanged(Field field, int context) {
+
+                switch (mDeviceState) {
+                    case idle:
+                        break;
+                    case recording:
+                        break;
+                    case starting:
+                        Logger.DIALOG("Audio recording starting");
+                        break;
+                    case stopping:
+                        Logger.DIALOG("Audio recording stopping...");
+                        break;
+                    case recordingComplete:
+                        acceptRecording();
+                        dismissScreen();
+                        break;
+                }
+            }
+        });
+        horizontalManager.add(mOkButton);
+
+        mCancelButton = new ButtonField("Cancel", Field.FIELD_RIGHT);
+        mOkButton.setMargin(4, 20, 4, 50);
+        mCancelButton.setChangeListener(new FieldChangeListener() {
+
+            public void fieldChanged(Field field, int context) {
+
+                switch (mDeviceState) {
+                    case idle:
+                        dismissScreen();
+                        break;
+                    case recording:
+                        haltRecording();
+                        deleteRecording(mCurrentFilename);
+                        dismissScreen();
+                        break;
+                    case starting:
+                        Logger.DIALOG("Audio recording starting");
+                        break;
+                    case stopping:
+                        Logger.DIALOG("Audio recording stopping...");
+                        break;
+                    case recordingComplete:
+                        deleteRecording(mCurrentFilename);
+                        dismissScreen();
+                }
+            }
+        });
+
+        horizontalManager.add(mCancelButton);
+    }
+
+    private void startRecording() {
+
+        synchronized (lockObject) {
+            mDeviceState = starting;
+            Logger.DEBUG("ice.audio - Idle -> Starting");
+            mRecorderThread = new AudioRecorderThread();
+            mRecorderThread.start();
+            mStartStopButton.setLabel("Stop Recording");
+        }
+    }
+
+    private void haltRecording() {
+
+        synchronized (lockObject) {
+            Logger.DEBUG("ice.audio - Recording->Stopping");
             mRecorderThread.stopRecording();
             mRecorderThread = null;
-
+            mStartStopButton.setLabel("Start Recording...");
         }
     }
 
-    public Object invoke( Object thiz, Object[] args) {
+    private void acceptRecording() {
 
-        Hashtable params = new Hashtable();
-        if (args.length == 2) { 
-            mFieldId = (String) args[0];
-         
-         // 'undefined' object indicates arguments not being passed 
-            if (! (args[1] instanceof Object)) { 
-                params = UploadUtilities.getNameValuePairHash((String)args[1], "=", "&");
-            } 
-            
-        } else { 
-        	Logger.ERROR("ice.audio - wrong number of arguments");
-            return Boolean.FALSE; 
-        }     
-        
-        NameValuePair temp; 
-        
-        temp = (NameValuePair) params.get("maxtime");
-        if (temp != null) {
-            mMaxDuration = Integer.parseInt( temp.getValue() ); 
-        } else { 
-            Logger.DEBUG("ice.audio indefinite recording");
+        // If this went ok, insert the hidden field. 		
+        try {
+            mController.insertHiddenFilenameScript(mFieldId, mCurrentFilename);
+            Logger.DEBUG("ice.audio - inserted audio clip in: " + mFieldId);
+        } catch (Exception e) {
+            Logger.DEBUG("Exception accepting recording: " + e);
         }
+    }
 
-        synchronized (lockObject) { 
-            try { 
-                if ( mDeviceState == idle) { 
-                    mDeviceState = starting; 
-                    Logger.DEBUG( "ice.audio - IDLE -> Starting");
-                    mRecorderThread =  new AudioRecorderThread();
-                    mRecorderThread.start(); 
 
-                } else if (mDeviceState == recording){
+    private void deleteRecording(String filename) {
 
-                    Logger.DEBUG( "ice.audio - Recording->Stopping");
-                    mRecorderThread.stopRecording();
-                    mRecorderThread = null;
-
-                } else if (mDeviceState == starting) { 
-                    Logger.DEBUG( "ice.audio - Still Starting");
-                } else if (mDeviceState == stopping) { 
-                    Logger.DEBUG( "ice.audio - Still Stopping");
-                }
-            } catch (Exception e) {  
-            	 Logger.ERROR("Exception recording audio: " + e);
+        FileConnection fc = null;
+        try {
+            fc = (FileConnection) Connector.open(filename);
+            if (fc.exists()) {
+                fc.delete();
             }
+
+        } catch (IOException ioe) {
+            Logger.ERROR("ice.audio - Error deleting temp file: " + ioe);
         }
-        return Boolean.TRUE;		
+    }
+
+    private void dismissScreen() {
+        try {
+            synchronized (UiApplication.getEventLock()) {
+                UiApplication.getUiApplication().popScreen(this);
+            }
+        } catch (Exception e) {
+            Logger.DEBUG("Exception dismissing screen: " + e);
+        }
+    }
+
+    private void enableCompletion() {
+        FileConnection fc = null;
+        long size = 0;
+        try {
+            fc = (FileConnection) Connector.open(mCurrentFilename);
+            if (fc.exists()) {
+                size = fc.fileSize();
+            }
+
+        } catch (IOException ioe) {
+            Logger.ERROR("ice.audio - Error deleting temp file: " + ioe);
+        }
+        AudioRecorder.this.mStatusField.setText("Audio file saved in: " + mCurrentFilename + " containing: " + size + " bytes");
+        mOkButton.setEnabled(true);
+    }
+
+    /**
+     * Called by the invoke method in the javascript
+     *
+     * @param fieldId
+     * @param maxDuration
+     */
+    public void setAudioParameters(String fieldId, int maxDuration) {
+
+        mFieldId = fieldId;
+        mMaxDuration = maxDuration;
+        mDeviceState = idle;
+        mOkButton.setEnabled(false);
+        mStartStopButton.setLabel("Start Recording");
+        mStatusField.setText("");
     }
 
 
-    class AudioRecorderThread extends Thread implements javax.microedition.media.PlayerListener { 
+    class AudioRecorderThread extends Thread implements
+            javax.microedition.media.PlayerListener {
 
-        private Player player; 
-        private RecordControl recordControl;		
+        private Player player;
+        private RecordControl recordControl;
 
 
         public void run() {
 
-            synchronized (lockObject) { 
+            synchronized (lockObject) {
 
-                try { 
+                try {
                     player = javax.microedition.media.Manager.createPlayer(
-                    "capture://audio?encoding=audio/amr" ); 
+                                                                                  "capture://audio?encoding=audio/amr");
 
-                    player.addPlayerListener(this); 
+                    player.addPlayerListener(this);
                     player.realize();
                     recordControl = (RecordControl) player.getControl("RecordControl");
-
-                    mCurrentFilename = FileUtils.getAudioFileURL("AudioRecording.amr");				
+                    mCurrentFilename = FileUtils.getAudioFileURL("AudioRecording.amr");
                     recordControl.setRecordLocation(mCurrentFilename);
 
-                    recordControl.startRecord(); 
-                    player.start();	
-                    Logger.DEBUG( "ice.audio - Starting -> Recording");
+                    recordControl.startRecord();
+                    player.start();
+                    Logger.DEBUG("ice.audio - Starting -> Recording");
 
-                    if (mMaxDuration > 0) { 
-                        mTerminatorThread = new Thread("Recording Terminator") { 
-                            public void run() { 
+                    if (mMaxDuration > 0) {
+                        mTerminatorThread = new Thread("Recording Terminator") {
+                            public void run() {
 
-                                try {  
-                                    Thread.sleep( mMaxDuration * 1000);
+                                try {
+                                    Thread.sleep(mMaxDuration * 1000);
                                     stopRecording();
-                                } catch (InterruptedException i) { }                                
+                                } catch (InterruptedException i) {
+                                }
                             }
                         };
 
                         mTerminatorThread.start();
-                    } 
+                    }
                     mDeviceState = recording;
 
-                } catch (Exception ioe) { 
-                	Logger.ERROR("ice.audio - Exception capturing audio: " + ioe);
+                } catch (Exception ioe) {
+                    Logger.ERROR("ice.audio - Exception capturing audio: " + ioe);
                 }
             }
         }
 
-        public void stopRecording() { 
+        public void stopRecording() {
 
-            synchronized (lockObject) { 
-                
+            synchronized (lockObject) {
+
                 mDeviceState = stopping;
-                if (mTerminatorThread != null) { 
-                    mTerminatorThread.interrupt(); 
+                if (mTerminatorThread != null) {
+                    mTerminatorThread.interrupt();
                 }
-                try { 
-                    if (player != null) { 
-                        player.close(); 
+                try {
+                    if (player != null) {
+                        player.close();
                         player = null;
                     }
 
-                    if (recordControl != null) { 
+                    if (recordControl != null) {
                         recordControl.stopRecord();
                         recordControl.commit();
-                        Logger.DEBUG( "ice.audio - Stopped");
-                        Logger.DEBUG( "ice.audio recorded in: " + mCurrentFilename);
+                        Logger.DEBUG("ice.audio - Stopped");
+                        Logger.DEBUG("ice.audio recorded in: " + mCurrentFilename);
 
-                        // If this went ok, insert the hidden field. 					
-                        mController.insertHiddenFilenameScript(mFieldId, mCurrentFilename);
-                        Logger.DEBUG( "ice.audio - inserted audio clip");
+                        enableCompletion();
+                        mDeviceState = recordingComplete;
+                        // save the file later, on demand
                     }
-                } catch (Exception e) { 
-                	Logger.ERROR("Exception stopping: " + e);
-                } finally { 
+                } catch (Exception e) {
+                    Logger.ERROR("Exception stopping: " + e);
+                    mDeviceState = idle;
+                } finally {
                     player = null;
                     recordControl = null;
-                    mDeviceState = idle;
                 }
             }
         }
 
-        public void playerUpdate(Player player, String event, Object eventData) {
+        public void playerUpdate(Player player, String event,
+                                 Object eventData) {
             // TODO Auto-generated method stub
 
         }
