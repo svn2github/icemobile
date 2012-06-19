cd6b638769f64e08ccf8f475c0f8dc2ff14c6531
judy.guglielmin <judy.guglielmin@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-06-19 23:51:48 +0000

    MOBI-246 initial commit
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@29554 1de00972-ac04-4911-a46b-06793c8d78d9

---
+++ jsf/components/component/src/org/icefaces/mobi/component/contentnavbar/ContentNavBar.java
@@ -0,0 +1,8 @@
+package org.icefaces.mobi.component.contentnavbar;
+
+
+public class ContentNavBar extends ContentNavBarBase {
+    public static final String CONTENTNAVBAR_BASE_CLASS = "mobi-contentnavbar-header";
+    public static final String CONTENTNAVBAR_BUTTON_CLASS = "mobi-contentnavbar-button";
+    public static final String CONTENTNAVBAR_BUTTON_MENU_CLASS = "mobi-contentnavbar-button menu";
+}
---
+++ jsf/components/component/src/org/icefaces/mobi/component/contentnavbar/ContentNavBarMeta.java
@@ -0,0 +1,36 @@
+package org.icefaces.mobi.component.contentnavbar;
+
+import org.icefaces.ace.meta.annotation.Component;
+import org.icefaces.ace.meta.annotation.Property;
+
+@Component(
+        tagName = "contentNavBar",
+        componentClass = "org.icefaces.mobi.component.contentnavbar.ContentNavBar",
+        rendererClass = "org.icefaces.mobi.component.contentnavbar.ContentNavBarRenderer",
+        generatedClass = "org.icefaces.mobi.component.contentnavbar.ContentNavBarBase",
+
+        componentType = "org.icefaces.ContentNavBar",
+        rendererType = "org.icefaces.ContentNavBarRenderer",
+        extendsClass = "javax.faces.component.UIPanel",
+        componentFamily = "org.icefaces.ContentNavBar",
+        tlddoc = "This mobility component renders a navigation tool bar for the contentPane" +
+                " component that will allow a Back button, and title.  A facet may be provided" +
+                " in future if required.  Must be a child of the contentPane. "
+)
+
+public class ContentNavBarMeta {
+    @Property(tlddoc = "style will be rendered on a root element of this component")
+    private String style;
+
+    @Property(tlddoc = "style class will be rendered on a root element of this component")
+    private String styleClass;
+
+    @Property(tlddoc = "if menuLabel is not null, then a menu button will be rendered on the tool bar." +
+            " A menu button will be styled and placed on this toolbar and a target is expected of the id" +
+            " for the menu's enclosing contentPane or home page contentPane to navigate through the stack.")
+    private String menuButtonLabel;
+
+    @Property(tlddoc = " required if using a menuButtonLabel or for home page navigation.  This requires the" +
+            " id of the contentPane that you wish to navigate to.")
+    private String menuButtonTargetId;
+}
