1beb0524c355084c99eb1e28a804c7fb0be21633
greg.dick <greg.dick@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-06-21 21:48:58 +0000

    MOBI-217 Initial check in
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@29631 1de00972-ac04-4911-a46b-06793c8d78d9

---
+++ samples/icemobilespring/src/main/java/org/icemobile/samples/springbasic/TabsetBean.java
@@ -0,0 +1,14 @@
+package org.icemobile.samples.springbasic;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+/**
+ * This is a sample backing bean for the MVC supported state
+ * The properties should be the same
+ */
+@SessionAttributes("tabsetBean")
+public class TabsetBean {
+
+
+}
