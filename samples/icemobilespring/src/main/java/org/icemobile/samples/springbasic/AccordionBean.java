90ebb4a58bdb6ecd1b454b884fc13f2992ec37c9
greg.dick <greg.dick@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-06-27 16:17:44 +0000

    MOBI-217 Initial Check in
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@29708 1de00972-ac04-4911-a46b-06793c8d78d9

---
+++ samples/icemobilespring/src/main/java/org/icemobile/samples/springbasic/AccordionBean.java
@@ -0,0 +1,13 @@
+package org.icemobile.samples.springbasic;
+
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+/**
+ * This is a sample backing bean for the MVC supported state
+ * The properties should be the same
+ */
+@SessionAttributes("accordionBean")
+public class AccordionBean {
+
+
+}
