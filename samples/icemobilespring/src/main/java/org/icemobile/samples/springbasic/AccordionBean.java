ccb22e480ea975f7e67676282c0bf95326ed1043
greg.dick <greg.dick@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-06-28 22:26:23 +0000

    MOBI-217 Improved sample by supporting the selected tab id
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@29756 1de00972-ac04-4911-a46b-06793c8d78d9

--- samples/icemobilespring/src/main/java/org/icemobile/samples/springbasic/AccordionBean.java
+++ samples/icemobilespring/src/main/java/org/icemobile/samples/springbasic/AccordionBean.java
@@ -4,10 +4,19 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 /**
  * This is a sample backing bean for the MVC supported state
- * The properties should be the same
+ * By convention, he name of the properties should be the same as the ids of
+ * the jsp tags.
  */
 @SessionAttributes("accordionBean")
 public class AccordionBean {
 
+    private String accordionOne = "tab2";
 
+    public String getAccordionOne() {
+        return accordionOne;
+    }
+
+    public void setAccordionOne(String accordionOne) {
+        this.accordionOne = accordionOne;
+    }
 }
