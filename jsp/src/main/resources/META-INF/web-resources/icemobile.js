efdde2a86361c853ffd8b932de2be8479ff8decc
steve.maryka <steve.maryka@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-06-28 23:44:01 +0000

    initial checkin of date spinner
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@29766 1de00972-ac04-4911-a46b-06793c8d78d9

--- jsp/src/main/resources/META-INF/web-resources/icemobile.js
+++ jsp/src/main/resources/META-INF/web-resources/icemobile.js
@@ -553,3 +553,300 @@ ice.mobi.accordionController = {
         }
     }
 }
+
+mobi.datespinner = {
+    pattern:{}, //supported formats are dd/MM/yyyy, MM-dd-yyyy, dd-MM-yyyy, yyyy-MM-dd, yyyy-dd-MM
+    opened:{},
+    centerCalculation:{},
+    scrollEvent:{},
+    init:function (clientId, yrSel, mSel, dSel, format) {
+	alter('Spinner init called');
+        var idPanel = clientId + "_bg";
+        if (!document.getElementById(idPanel).className) {
+            document.getElementById(idPanel).className = 'mobi-date-bg-inv';
+        }
+        var intDt = parseInt(dSel);
+        var intMth = parseInt(mSel);
+        var intYr = parseInt(yrSel);
+        if (format) {
+            this.pattern[clientId] = format;
+            //           ice.log.debug(ice.log, ' pattern changed to ='+this.pattern);
+        }
+        this.opened[clientId] = false;
+        //have to set the value controls to the correct integer
+        var mnthEl = document.getElementById(clientId + "_mInt");
+        var yrEl = document.getElementById(clientId + "_yInt");
+        var dateEl = document.getElementById(clientId + "_dInt");
+        if (mnthEl) {
+            mnthEl.innerHTML = intMth;
+        }
+        if (yrEl) {
+            yrEl.innerHTML = intYr;
+        }
+        if (dateEl) {
+            dateEl.innerHTML = intDt;
+        }
+        this.updateDate(clientId);
+        this.scrollEvent = 'ontouchstart' in window ? "touchmove" : "scroll";
+    },
+    mUp:function (clientId) {
+        var mId = clientId + '_mInt';
+        var mnthEl = document.getElementById(mId);
+        if (mnthEl) {
+            var mInt = this.getIntValue(mId);
+            if (mInt == 12) {
+                mnthEl.innerHTML = 1;
+            }
+            else {
+                mnthEl.innerHTML = mInt + 1;
+            }
+        }
+        this.updateDate(clientId);
+    },
+    mDn:function (clientId) {
+        var mId = clientId + '_mInt';
+        var mnthEl = document.getElementById(mId);
+        if (mnthEl) {
+            var mInt = this.getIntValue(mId);
+            if (mInt == 1) {
+                mnthEl.innerHTML = 12;
+            }
+            else {
+                mnthEl.innerHTML = mInt - 1;
+            }
+        }
+        this.updateDate(clientId);
+    },
+    yUp:function (clientId, yrMin, yrMax) {
+        var yId = clientId + '_yInt';
+        var yrEl = document.getElementById(yId);
+        if (yrEl) {
+            var yInt = this.getIntValue(yId);
+            if (yInt == yrMax) {
+                //nothing
+            }
+            else {
+                yrEl.innerHTML = yInt + 1;
+            }
+        }
+        this.updateDate(clientId);
+    },
+    yDn:function (clientId, yrMin, yrMax) {
+        var yrEl = document.getElementById(clientId + '_yInt');
+        if (yrEl) {
+            yInt = this.getIntValue(clientId + '_yInt');
+            if (yInt == yrMin) {
+                //nothing
+            }
+            else {
+                yrEl.innerHTML = yInt - 1;
+            }
+        }
+        this.updateDate(clientId);
+    },
+    dUp:function (clientId) {
+        var dId = clientId + '_dInt';
+        var dEl = document.getElementById(dId);
+        var mInt = this.getIntValue(clientId + "_mInt");
+        var yInt = this.getIntValue(clientId + "_yInt");
+        var dInt = this.getIntValue(dId);
+        var numDaysInMonth = this.daysInMonth(mInt, yInt);
+        if (dEl) {
+            if (dInt >= numDaysInMonth) {
+                dEl.innerHTML = 1;
+            }
+            else {
+                dEl.innerHTML = dInt + 1;
+            }
+        }
+        this.updateDate(clientId);
+    },
+    dDn:function (clientId) {
+        var dId = clientId + '_dInt';
+        var dEl = document.getElementById(dId);
+        var mInt = this.getIntValue(clientId + "_mInt");
+        var yInt = this.getIntValue(clientId + "_yInt");
+        var dInt = this.getIntValue(dId);
+        var numDaysInMonth = this.daysInMonth(mInt, yInt);
+        if (dEl) {
+            if (dInt == 1 || dInt > numDaysInMonth) {
+                dEl.innerHTML = numDaysInMonth;
+            }
+            else {
+                dEl.innerHTML = dInt - 1;
+            }
+        }
+        this.updateDate(clientId);
+    },
+    updateDate:function (clientId) {
+        var dId = clientId + "_dInt";
+        var dEl = document.getElementById(dId);
+        var mInt = this.getIntValue(clientId + "_mInt");
+        var yInt = this.getIntValue(clientId + "_yInt");
+        var dInt = this.getIntValue(dId);
+        var upDate = this.validate(yInt, mInt, dInt);
+        if (!upDate) {
+            dInt = this.daysInMonth(mInt, yInt);
+            dEl.innerHTML = dInt;
+        }
+        this.writeTitle(clientId, dInt, mInt, yInt);
+    },
+
+    writeTitle:function (clientId, iD, iM, iY) {
+        var date = new Date();
+        date.setDate(iD);
+        date.setMonth(iM - 1);
+        date.setYear(iY);
+        var titleEl = document.getElementById(clientId + '_title');
+        titleEl.innerHTML = date.toDateString();
+    },
+
+    daysInMonth:function (iMnth, iYr) {
+        var aDate = new Date(iYr, iMnth, 0);
+        return aDate.getDate();
+    },
+
+    validate:function (iY, iM, iD) {
+        if (iY != parseInt(iY, 10) || iM != parseInt(iM, 10) || iD != parseInt(iD, 10)) return false;
+        iM--;
+        var newDate = new Date(iY, iM, iD);
+        if ((iY == newDate.getFullYear()) && (iM == newDate.getMonth()) && (iD == newDate.getDate())) {
+            return newDate;
+        }
+        else return false;
+    },
+
+    getIntValue:function (id) {
+        var element = document.getElementById(id);
+        if (element) {
+            var stringEl = element.innerHTML;
+            return parseInt(stringEl);
+        } else return 1;
+    },
+    select:function (clientId, cfg) {
+        //
+        var inputEl = document.getElementById(clientId + '_input');
+        var hiddenEl = document.getElementById(clientId + '_hidden');
+        var dInt = this.getIntValue(clientId + "_dInt");
+        var mInt = this.getIntValue(clientId + "_mInt");
+        var yInt = this.getIntValue(clientId + "_yInt");
+        var dStr = dInt;
+        var mStr = mInt;
+        if (dInt < 10) {
+            dStr = '0' + dInt;
+        }
+        if (mInt < 10) {
+            mStr = '0' + mInt;
+        }
+        // default to american MM/dd/yyyy, pattern
+        var dateStr = mStr + '/' + dStr + '/' + yInt;
+        var myPattern = this.pattern[clientId];
+        // compare '-' dash delimiter
+        if (myPattern == 'MM-dd-yyyy') {
+            dateStr = mStr + '-' + dStr + '-' + yInt;
+        } else if (myPattern == 'yyyy-MM-dd') {
+            dateStr = yInt + "-" + mStr + "-" + dStr;
+        } else if (myPattern == 'yyyy-dd-MM') {
+            dateStr = yInt + "-" + dStr + "-" + mStr;
+        } else if (myPattern == 'dd-MM-yyyy') {
+            dateStr = dStr + '-' + mStr + '-' + yInt;
+        }
+        // compare '/' dash delimiter
+        else if (myPattern == 'MM/dd/yyyy') {
+            dateStr = mStr + '/' + dStr + '/' + yInt;
+        } else if (myPattern == 'yyyy/MM/dd') {
+            dateStr = yInt + "/" + mStr + "/" + dStr;
+        } else if (myPattern == 'yyyy/dd/MM') {
+            dateStr = yInt + "/" + dStr + "/" + mStr;
+        } else if (myPattern == 'dd/MM/yyyy') {
+            dateStr = dStr + '/' + mStr + '/' + yInt;
+        }
+        hiddenEl.value = dateStr;
+        inputEl.value = dateStr;
+
+        this.dateSubmit(cfg, clientId);
+        this.close(clientId);
+    },
+    dateSubmit: function(cfg, clientId) {
+            this.cfg = cfg;
+            var singleSubmit = this.cfg.singleSubmit;
+            var event = this.cfg.event;
+            var hasBehaviors = false;
+            var behaviors = this.cfg.behaviors;
+            if (behaviors) {
+                hasBehaviors = true;
+            }
+            if (hasBehaviors) {
+                if (behaviors.change) {
+                    behaviors.change();
+                }
+            }
+            if (!hasBehaviors && singleSubmit) {
+		alert('Bad shit. Calling ice.se');
+                ice.se(event, clientId);
+            }
+    },
+    inputSubmit: function(clientId, cfg){
+        if (this.opened[clientId]==true){
+            return;
+        }
+        var hiddenEl = document.getElementById(clientId + '_hidden');
+        var inputEl = document.getElementById(clientId + '_input');
+        hiddenEl.value= inputEl.value;
+        this.dateSubmit(cfg, clientId);
+    },
+    toggle:function (clientId) {
+        if (!this.opened[clientId]) {
+            this.open(clientId);
+        } else {
+            this.close(clientId);
+        }
+    },
+    open:function (clientId) {
+        var idPanel = clientId + "_bg";
+        var idPopPanel = clientId + "_popup";
+
+        // add scroll listener
+        this.centerCalculation[clientId] = function () {
+            mobi.panelAutoCenter(idPopPanel);
+        };
+
+        if (window.addEventListener) {
+            window.addEventListener(this.scrollEvent, this.centerCalculation[clientId], false);
+            window.addEventListener('resize', this.centerCalculation[clientId], false);
+        } else { // older ie event listener
+            window.attachEvent(this.scrollEvent, this.centerCalculation[clientId]);
+            window.attachEvent("resize", this.centerCalculation[clientId]);
+        }
+        // add visible style classes
+        document.getElementById(idPanel).className = "mobi-date-bg";
+        document.getElementById(idPopPanel).className = "mobi-date-container";
+        // set as visible.
+        this.opened[clientId] = true;
+        // calculate center for first view
+        mobi.panelAutoCenter(idPopPanel);
+    },
+    close:function (clientId) {
+        var idPanel = clientId + "_bg";
+
+        // remove scroll listener
+        if (window.removeEventListener) {
+            window.removeEventListener(this.scrollEvent, this.centerCalculation[clientId], false);
+            window.removeEventListener('resize', this.centerCalculation[clientId], false);
+        } else { // older ie cleanup
+            window.detachEvent(this.scrollEvent, this.centerCalculation[clientId], false);
+            window.detachEvent('resize', this.centerCalculation[clientId], false);
+        }
+        // hide the dialog
+        document.getElementById(idPanel).className = "mobi-date-bg-inv";
+        document.getElementById(clientId + "_popup").className = "mobi-date-container-inv";
+        this.opened[clientId] = false;
+        this.centerCalculation[clientId] = undefined;
+    },
+    unload:function (clientId) {
+        /* var titleEl = document.getElementById(clientId+'_title');
+         titleEl.innerHTML = "";  */
+        this.pattern[clientId] = null;
+        this.opened[clientId] = null;
+    }
+}
