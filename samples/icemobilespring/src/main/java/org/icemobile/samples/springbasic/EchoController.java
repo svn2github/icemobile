0172ae32814f30cc698b5a07da94f1001f4c0482
ted.goddard <ted.goddard@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-07-06 22:12:25 +0000

    removing redundant controllers (MOBI-217)
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@29850 1de00972-ac04-4911-a46b-06793c8d78d9

---
+++ samples/icemobilespring/src/main/java/org/icemobile/samples/springbasic/EchoController.java
@@ -0,0 +1,67 @@
+package org.icemobile.samples.springbasic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * General Controller for echoing simple input pages
+ */
+@Controller
+public class EchoController {
+
+	@ModelAttribute
+	public void ajaxAttribute(WebRequest request, Model model) {
+		model.addAttribute("ajaxRequest", AjaxUtils.isAjaxRequest(request));
+	}
+
+    @RequestMapping(value = "/accordion")
+    public void doRequest(
+            @ModelAttribute("accordionBean" )AccordionBean model) {
+    }
+
+    @RequestMapping(value = "/carousel")
+    public void doRequest(
+            @ModelAttribute("carouselBean") CarouselBean model) {
+    }
+
+    @RequestMapping(value = "/datetime")
+    public void doRequest(
+            @ModelAttribute("dateTimeSpinnerBean")
+                    DateTimeSpinnerBean model) {
+    }
+
+    @RequestMapping(value = "/flipswitch")
+    public void doRequest(
+            @ModelAttribute("flipSwitchBean") FlipSwitchBean model) {
+    }
+
+    @RequestMapping(value = "/geolocation")
+    public void doRequest(
+            @ModelAttribute("geolocationBean") GeolocationBean model) {
+    }
+
+    @RequestMapping(value = "/inputtext")
+    public void doRequest(
+            @ModelAttribute("inputTextBean") InputTextBean model) {
+    }
+
+    @RequestMapping(value = "/list")
+    public void doRequest(@ModelAttribute("ListBean") ListBean model) {
+    }
+
+    @RequestMapping(value = "/qrscan")
+    public void doRequest(@ModelAttribute("QRScanBean")QRScanBean model) {
+    }
+
+    @RequestMapping(value = "/tabset")
+    public void doRequest(@ModelAttribute("tabsetBean") TabsetBean model) {
+    }
+
+}
+
