7516cc45c44e2224616047cdacf999c3d5c22bc4
greg.dick <greg.dick@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-07-09 20:31:18 +0000

    MOBI-217 Initial Check-in
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@29875 1de00972-ac04-4911-a46b-06793c8d78d9

---
+++ jsp/src/main/java/org/icemobile/jsp/tags/PagePanelHeaderTag.java
@@ -0,0 +1,56 @@
+package org.icemobile.jsp.tags;
+
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.tagext.TagSupport;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.logging.Logger;
+
+/**
+ *
+ */
+public class PagePanelHeaderTag extends TagSupport {
+
+    public static final String HEADER_CLASS = "mobi-pagePanel-header";
+
+    private static Logger LOG = Logger.getLogger(PagePanelHeaderTag.class.getName());
+
+    public int doStartTag() throws JspTagException {
+
+        Writer out = pageContext.getOut();
+
+        StringBuilder tag = new StringBuilder(TagUtil.DIV_TAG);
+        tag.append(" id=\"").append(getId()).append("_pgPnlHdr\"");
+        tag.append(" class=\"").append(HEADER_CLASS).append("\"");
+        tag.append(">");
+
+        try {
+            out.write(tag.toString());
+        } catch (IOException ioe) {
+            LOG.severe("IOException writing PagePanelHeader: " + ioe);
+        }
+        return EVAL_BODY_INCLUDE;
+    }
+
+
+    public int doEndTag() throws JspTagException {
+
+        Writer out = pageContext.getOut();
+        try {
+            out.write(TagUtil.DIV_TAG_END);
+        } catch (IOException ioe) {
+            LOG.severe("IOException closing PagePanelHeader: " + ioe);
+        }
+        return EVAL_PAGE;
+    }
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
