4361f974ae3023289bdae29d38916cc9d122ffa4
greg.dick <greg.dick@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-07-09 20:49:52 +0000

    MOBI-217 Added tabsetOne property as selected index
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@29884 1de00972-ac04-4911-a46b-06793c8d78d9

--- samples/icemobilespring/src/main/java/org/icemobile/samples/springbasic/TabsetBean.java
+++ samples/icemobilespring/src/main/java/org/icemobile/samples/springbasic/TabsetBean.java
@@ -4,11 +4,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 /**
- * This is a sample backing bean for the MVC supported state
- * The properties should be the same
+ * This is a sample backing bean for the MVC supported Tabset
  */
 @SessionAttributes("tabsetBean")
 public class TabsetBean {
 
 
+    private int tabsetOneIndex;
+
+    public int getTabsetOne() {
+        return tabsetOneIndex;
+    }
+
+    public void setTabsetOne(int tabsetOneIndex) {
+        this.tabsetOneIndex = tabsetOneIndex;
+    }
 }
