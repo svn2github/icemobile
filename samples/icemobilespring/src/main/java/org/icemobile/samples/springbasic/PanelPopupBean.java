6c59efdc60f2e64077c219d7c48d4be444bcb457
greg.dick <greg.dick@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-07-13 23:28:25 +0000

    MOBI-217 Initial check in
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@29980 1de00972-ac04-4911-a46b-06793c8d78d9

---
+++ samples/icemobilespring/src/main/java/org/icemobile/samples/springbasic/PanelPopupBean.java
@@ -0,0 +1,21 @@
+package org.icemobile.samples.springbasic;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ *
+ */
+public class PanelPopupBean {
+
+    private boolean visible;
+
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+}
