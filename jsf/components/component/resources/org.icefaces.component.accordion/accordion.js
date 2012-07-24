4683a22433dfdabf6d7d2eba5bda7a73f344428e
greg.dick <greg.dick@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-07-24 15:16:41 +0000

    MOBI-277 Fixed height calculation
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@30192 1de00972-ac04-4911-a46b-06793c8d78d9

--- jsf/components/component/resources/org.icefaces.component.accordion/accordion.js
+++ jsf/components/component/resources/org.icefaces.component.accordion/accordion.js
@@ -1,26 +1,28 @@
- (function() {
+(function() {
     //functions that do not encapsulate any state, they just work with the provided parameters
     //and globally accessible variables
     //---------------------------------------
     function updateHidden(clientId, value) {
-        var hidden = document.getElementById(clientId+"_hidden");
+        var hidden = document.getElementById(clientId + "_hidden");
         if (hidden) {
             hidden.value = value;
         }
     }
-    function getDivHeight(clientId){
+
+    function getDivHeight(clientId) {
         return document.getElementById(clientId).innerHeight;
     }
-    function updateHeightInOpenClass (ruleName, height){
-        if (document.styleSheets){
-            for (var i=0; i< document.styleSheets.length; i++){
+
+    function updateHeightInOpenClass(ruleName, height) {
+        if (document.styleSheets) {
+            for (var i = 0; i < document.styleSheets.length; i++) {
                 var styleSh = document.styleSheets[i];
                 var index = 0;
                 var found = false;
                 var cssRule;
                 do {
                     if (styleSh.cssRules) {
-                        cssRule = styleSh.cssRules[index] ;
+                        cssRule = styleSh.cssRules[index];
                     }
                     else {
                         cssRule = styleSh.rules[index];
@@ -38,18 +40,19 @@
             }
         }
     }
-    function calcMaxDivHeight(clientId){
+
+    function calcMaxDivHeight(clientId) {
         var accord = document.getElementById(clientId);
         var mxht = 0;
         //find all sections of the clientId and calc height.  set maxheight and height to max height of the divs
-        var children =  document.getElementById(clientId).getElementsByTagName('section');
-        for (var i= 0; i < children.length; i++){
-           if ( children[0].scrollHeight > mxht) {
-               mxht= children[0].scrollHeight;
-           }
+        var children = document.getElementById(clientId).getElementsByTagName('section');
+        for (var i = 0; i < children.length; i++) {
+            if (children[i].scrollHeight > mxht) {
+                mxht = children[i].scrollHeight;
+            }
         }
-        if (mxht <= 33){
-            mxht = accord.clientHeight-33;
+        if (mxht <= 33) {
+            mxht = accord.clientHeight - 33;
         }
         return mxht;
     }
@@ -64,38 +67,40 @@
         var openClass = ".mobi-accordion .open";
         var myclient = clientId;
         var paneOpId = cfgIn.opened || null;
-      //  var scrollEvent = 'ontouchstart' in window ? "touchmove" : "scroll";
+        //  var scrollEvent = 'ontouchstart' in window ? "touchmove" : "scroll";
         var autoheight = cfgIn.autoheight || false;
         var maxheight = cfgIn.maxheight || "150px";
-        if (autoheight == true){
+        if (autoheight == true) {
             var calcht = calcMaxDivHeight(clientId)
-            if (calcht > 0) maxheight = calcht ;
+            if (calcht > 0) {
+                maxheight = calcht;
+            }
         }
-        var heightString = maxheight+"px";
+        var heightString = maxheight + "px";
         updateHeightInOpenClass(openClass, heightString);
         return {
             toggle: function(el, cached) {
                 var theParent = el.parentElement;
-                if (!theParent){
+                if (!theParent) {
                     theParent = el.parentNode; //mozilla
                 }
                 //which child is the element?
                 updateHidden(myclient, theParent.id);
-                if (!cached){
+                if (!cached) {
                     ice.se(null, myclient);
                 }
                 var alreadyOpen = paneOpId;
-                if (alreadyOpen && alreadyOpen !== theParent.id)  {
+                if (alreadyOpen && alreadyOpen !== theParent.id) {
                     var openedEl = document.getElementById(alreadyOpen);
-                    if (openedEl){
+                    if (openedEl) {
                         document.getElementById(alreadyOpen).className = 'closed';
                     }
                 }
                 if ('open' === theParent.className) {
-                    theParent.className='closed';
+                    theParent.className = 'closed';
                 } else {
                     theParent.className = 'open';
-                    paneOpId =  theParent.id;
+                    paneOpId = theParent.id;
                 }
 
             },
@@ -103,36 +108,37 @@
                 cfgObj = cfgIn; //not sure I need to keep this?
                 //server may want to push new dynamic values for maxheight and autoheight
                 var change = false;
-                if (autoheight!=cfgUpd.autoheight){
+                if (autoheight != cfgUpd.autoheight) {
                     autoheight = cfgUpd.autoheight || false;
                     change = true;
                 }
-                if (maxheight!=cfgUpd.maxheight){
+                if (maxheight != cfgUpd.maxheight) {
                     maxheight = cfgUpd.maxheight || "200px";
                     change = true;
                 }
-                if (change==true){
+                if (change == true) {
                     var newHeight = calcMaxDivHeight(clientId);
                 }
                 paneOpId = cfgIn.opened || null;
             }
-         }
+        }
     }
+
     mobi.accordionController = {
         panels: {},
         initClient: function(clientId, cfg) {
-            if (!this.panels[clientId]){
+            if (!this.panels[clientId]) {
                 this.panels[clientId] = Accordion(clientId, cfg);
             } else {
                 this.panels[clientId].updateProperties(clientId, cfg);
             }
         },
-        toggleClient: function(clientId, el, cachetyp){
-            if (this.panels[clientId]){
+        toggleClient: function(clientId, el, cachetyp) {
+            if (this.panels[clientId]) {
                 this.panels[clientId].toggle(el, cachetyp);
             }
         }
     }
 
-  })();
+})();
 
