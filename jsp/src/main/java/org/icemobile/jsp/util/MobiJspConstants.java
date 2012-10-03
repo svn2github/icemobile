0bb0285ea79f01ae50f307e424ebfa4921849ea3
philip.breau <philip.breau@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-10-03 18:10:22 +0000

    add jsp constants
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@31278 1de00972-ac04-4911-a46b-06793c8d78d9

---
+++ jsp/src/main/java/org/icemobile/jsp/util/MobiJspConstants.java
@@ -0,0 +1,8 @@
+package org.icemobile.jsp.util;
+
+public class MobiJspConstants {
+	
+	public final static String RESOURCE_BASE_URL = "/icemobile-resources";
+	public final static String JAR_RESOURCE_PATH = "META-INF/web-resources";
+
+}
