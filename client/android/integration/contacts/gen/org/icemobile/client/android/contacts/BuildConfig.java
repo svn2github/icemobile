c92b1d5b90043c9729ab79e56d6dd30ac2cc5d83
greg.dick <greg.dick@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-10-04 17:09:42 +0000

    Mobi-238 Contact list support
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@31313 1de00972-ac04-4911-a46b-06793c8d78d9

---
+++ client/android/integration/contacts/gen/org/icemobile/client/android/contacts/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.icemobile.client.android.contacts;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ client/android/integration/contacts/src/org/icemobile/client/android/contacts/ContactActivity.java
@@ -0,0 +1,15 @@
+package org.icemobile.client.android.contacts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ContactActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
