df6e4ebb0866f2186985fcbdba8b8d010d11ad51
philip.breau <philip.breau@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-10-04 13:12:40 +0000

    MOBI-350 Extended <mobi:deviceResource>
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@31297 1de00972-ac04-4911-a46b-06793c8d78d9

--- core/src/main/java/org/icemobile/util/ClientDescriptor.java
+++ core/src/main/java/org/icemobile/util/ClientDescriptor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2012 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.icemobile.util;
 
 import java.util.logging.Logger;
@@ -5,138 +20,153 @@ import java.util.logging.Logger;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * A central client feature and detection class for ICEmobile. 
+ * Will detect the OS and 
+ *
+ */
 public class ClientDescriptor {
+    
+    private final static String SESSION_KEY = "mobiClient";
     private final static String HEADER_ACCEPT = "Accept";
     private final static String HEADER_ACCEPT_BLACKBERRY_EMUL = "vnd.rim";  //found when emulating IE or FF on BB
     private final static String COOKIE_NAME_ICEMOBILE_CONTAINER = "com.icesoft.user-agent";
     private final static String COOKIE_VALUE_ICEMOBILE_CONTAINER = "HyperBrowser";
-    private final static String SESSION_KEY_SX = "iceAuxRequestMap";
 
     private static Logger log = Logger.getLogger(ClientDescriptor.class.getName());
     
-    private String userAgent;
+    private String userAgent = "";
     private String httpAccepted;
     
-    private DeviceType deviceType;
-    private boolean handheld;
-    private boolean tablet;
+    public enum OS{ WINDOWS, IOS, MAC, ANDROID, BLACKBERRY, LINUX }
+    public enum FORM_FACTOR{ HANDHELD, TABLET, DESKTOP }
+    
+    private OS os;
+    private FORM_FACTOR formFactor;
     private boolean icemobileContainer;
-    private boolean sx;
+    private boolean sxRegistered;
     
     private UserAgentInfo _userAgentInfo;
     
-    public ClientDescriptor(HttpServletRequest request){
-        updateUserAgent(request);
+    private ClientDescriptor(HttpServletRequest request){
+        System.out.println(this.getClass().getName()+" ctor****");
         updateHttpAccepted(request);
+        updateUserAgent(request);
+        updateOS();
+        updateFormFactor();
         updateICEmobileContainer(request);
-        updateSXContainer(request);
+        updateSXRegistered(request);
+        request.getSession().setAttribute(SESSION_KEY, this);
+    }
+    
+    public static ClientDescriptor getInstance(HttpServletRequest request){
+        System.out.println("session id="+request.getSession().getId());
+        System.out.println("ua="+request.getHeader("User-Agent"));
+        for( Cookie cookie : request.getCookies()){
+            System.out.println("cookie: "+cookie.getName()+"="+cookie.getValue());
+        }
+        
+        ClientDescriptor cd = (ClientDescriptor)request.getSession().getAttribute(SESSION_KEY);
+        //always update if user agent changed
+        if( cd == null || cd.isUserAgentUpdateRequired(request)){
+            cd = new ClientDescriptor(request);
+        }
+        //check if sx has been newly registered
+        if( !cd.isSXRegistered() ){
+            cd.updateSXRegistered(request);
+        }
+        return cd;        
+    }
+    
+    private boolean isUserAgentUpdateRequired(HttpServletRequest request){
+        if( !userAgent.equals(request.getHeader("User-Agent"))){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     public String getUserAgent() {
         return userAgent;
     }
 
-    public DeviceType getDeviceType() {
-        return deviceType;
+    public boolean isHandheldBrowser() {
+        return FORM_FACTOR.HANDHELD == formFactor;
     }
 
-    public boolean isHandheld() {
-        return handheld;
-    }
-
-    public boolean isTablet() {
-        return tablet;
+    public boolean isTabletBrowser() {
+        return FORM_FACTOR.TABLET == formFactor;
     }
     
-    private void updateDeviceType(){
+    private void updateOS(){
         if( _userAgentInfo != null ){
-            if (_userAgentInfo.isIphone()) {
-                deviceType = DeviceType.IPHONE;
+            if (_userAgentInfo.isIOS()) {
+                os = OS.IOS;
             }
-            if (_userAgentInfo.isAndroidTablet()) {
-                deviceType = DeviceType.ANDROID_TABLET;
+            else if (_userAgentInfo.isAndroidOS()){
+                os = OS.ANDROID;
             }
-            if (_userAgentInfo.isAndroidPhone()) {
-                deviceType = DeviceType.ANDROID_PHONE;
+            else if (_userAgentInfo.isBlackberryOS() || httpAccepted.contains(HEADER_ACCEPT_BLACKBERRY_EMUL)) {
+                os = OS.BLACKBERRY;
             }
-            if (_userAgentInfo.isBlackberry() || httpAccepted.contains(HEADER_ACCEPT_BLACKBERRY_EMUL)) {
-                deviceType = DeviceType.BLACKBERRY;
+            else if( _userAgentInfo.isMacOS()){
+                os = OS.MAC;
             }
-            if (_userAgentInfo.isIpad()) {
-                deviceType = DeviceType.IPAD;
+            else if( _userAgentInfo.isWindowsOS()){
+                os = OS.WINDOWS;
             }
+            else{
+                os = OS.LINUX;
+            }
+            System.out.println("os="+os);
         }
     }
 
-    /**
-     * Test for Android
-     * 
-     * @return true if client is Android
-     */
-    public boolean isAndroidPhone() {
-        return _userAgentInfo.isAndroidPhone();
+    public boolean isAndroidOS() {
+        return os == OS.ANDROID;
     }
 
-    /**
-     * Test for BlackBerry
-     * 
-     * @return true if client is BlackBerry
-     */
-    public boolean isBlackBerry() {
-        return _userAgentInfo.isBlackberry();
+    public boolean isBlackBerryOS() {
+        return os == OS.BLACKBERRY;
     }
 
-    /**
-     * Test for Internet Explorerer
-     * 
-     * @return true if client is Internet Explorer
-     */
-    public boolean isIE() {
+    public boolean isIEBrowser() {
         return _userAgentInfo.isIE();
     }
 
-    /**
-     * Test for iOS
-     * 
-     * @return true if client is iOS
-     */
     public boolean isIOS() {
-        return _userAgentInfo.isIOS();
+        return os == OS.IOS;
     }
 
-    /**
-     * Some input components may have html5 support for iOS5 such as DateSpinner
-     * 
-     * @return true if request header denotes os 5_0
-     */
     public boolean isIOS5() {
-        return _userAgentInfo.isIOS5();
+        return os == OS.IOS && _userAgentInfo.isIOS5();
     }
 
-    /**
-     * @return true if request header denotes os 6_0
-     */
     public boolean isIOS6() {
-        return _userAgentInfo.isIOS6();
+        return os == OS.IOS && _userAgentInfo.isIOS6();
     }
     
-    public boolean isSX(){
-        return sx;
+    public boolean isSXRegistered(){
+        return sxRegistered;
     }
     
     public boolean isICEmobileContainer(){
         return icemobileContainer;
     }
     
+    public boolean isDesktopBrowser(){
+        return formFactor == FORM_FACTOR.DESKTOP;
+    }
+    
     private void updateUserAgent(HttpServletRequest request){
-        userAgent = request.getHeader("User-Agent");
-        if( userAgent == null || userAgent.length() == 0 ){
+        String ua = request.getHeader("User-Agent");
+        if( ua == null || ua.length() == 0 ){
             log.warning("Could not detect device settings, user agent is null");
         }
         else{
-            tablet = _userAgentInfo.isTabletBrowser();
-            handheld = _userAgentInfo.isMobileBrowser();
-            updateDeviceType();
+            userAgent = ua;
+            _userAgentInfo = new UserAgentInfo(userAgent);
         }
     }
 
@@ -155,8 +185,23 @@ public class ClientDescriptor {
         }
     }
     
-    private void updateSXContainer(HttpServletRequest request){
-        sx = request.getSession().getAttribute(SESSION_KEY_SX) != null;
+    private void updateSXRegistered(HttpServletRequest request){
+        sxRegistered = request.getSession().getAttribute(Constants.SESSION_KEY_SX) != null;
+        System.out.println("*************** sx="+sxRegistered);
+    }
+    
+    private void updateFormFactor(){
+        if( _userAgentInfo != null ){
+            if (_userAgentInfo.isTabletBrowser()) {
+                formFactor = FORM_FACTOR.TABLET;
+            }
+            if (_userAgentInfo.isMobileBrowser()){
+                formFactor = FORM_FACTOR.HANDHELD;
+            }
+            else {
+                formFactor = FORM_FACTOR.DESKTOP;
+            }
+        }
     }
 
 }
