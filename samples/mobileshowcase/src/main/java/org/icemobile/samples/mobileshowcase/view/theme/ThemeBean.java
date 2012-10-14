3d7799853f739c181a268585b2bfce941097f1cb
philip.breau <philip.breau@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-10-14 23:02:19 +0000

    MOBI-389 Add more layout components to demos - add menuButton for theme switcher
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@31466 1de00972-ac04-4911-a46b-06793c8d78d9

--- samples/mobileshowcase/src/main/java/org/icemobile/samples/mobileshowcase/view/theme/ThemeBean.java
+++ samples/mobileshowcase/src/main/java/org/icemobile/samples/mobileshowcase/view/theme/ThemeBean.java
@@ -16,9 +16,14 @@
 
 package org.icemobile.samples.mobileshowcase.view.theme;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import java.io.Serializable;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 
 /**
  * Simple Model for keeping track of the current them.  The class also handles
@@ -29,13 +34,64 @@ import java.io.Serializable;
 public class ThemeBean implements Serializable {
 
     // empty theme name will default to auto on the select menu
-    private String currentTheme = "";
+    private ThemeData currentTheme;
+    private List<ThemeData> largeThemes;
+    private List<ThemeData> smallThemes;
+    
+    public ThemeBean(){
+        largeThemes = new ArrayList<ThemeData>();
+        largeThemes.add(new ThemeData("","Auto"));
+        largeThemes.add(new ThemeData("ipad","iPad"));
+        largeThemes.add(new ThemeData("honeycomb","Android"));
+        
+        smallThemes = new ArrayList<ThemeData>();
+        smallThemes.add(new ThemeData("","Auto"));
+        smallThemes.add(new ThemeData("iphone","iPhone"));
+        smallThemes.add(new ThemeData("android","Android"));
+        smallThemes.add(new ThemeData("bberry","Blackberry"));
+    }
 
-    public String getCurrentTheme() {
+    public ThemeData getCurrentTheme() {
         return currentTheme;
     }
 
-    public void setCurrentTheme(String currentTheme) {
+    public void setCurrentTheme(ThemeData currentTheme) {
         this.currentTheme = currentTheme;
+    }
+    
+    public class ThemeData implements Serializable{
+        private String name;
+        private String label;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getLabel() {
+            return label;
+        }
+        public void setLabel(String label) {
+            this.label = label;
+        }
+        public ThemeData(String name, String label) {
+            super();
+            this.name = name;
+            this.label = label;
+        }
+        
+        public String setTheme(){
+            currentTheme = this;
+            return "showcase?faces-redirect=true";
+        }
+        
+    }
+
+    public List<ThemeData> getLargeThemes() {
+        return largeThemes;
+    }
+
+    public List<ThemeData> getSmallThemes() {
+        return smallThemes;
     }
 }
