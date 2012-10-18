24e742047a1886b3338e810fd4992236d11b6539
greg.dick <greg.dick@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-10-18 00:06:40 +0000

    MOBI-429 Initial check in of javascript logging extension
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@31562 1de00972-ac04-4911-a46b-06793c8d78d9

---
+++ client/android/integration/util/src/org/icemobile/client/android/JavascriptLoggerInterface.java
@@ -0,0 +1,28 @@
+package org.icemobile.client.android;
+
+import android.util.Log;
+
+public class JavascriptLoggerInterface implements JavascriptInterface {
+
+    private final String JAVASCRIPT_TAG = "Javascript layer";
+
+    public void logInContainer(String message, int category) {
+        switch (category) {
+            case Log.ERROR:
+                Log.e(JAVASCRIPT_TAG, message);
+                break;
+            case Log.DEBUG:
+                Log.d(JAVASCRIPT_TAG, message);
+                break;
+            case Log.WARN:
+                Log.w(JAVASCRIPT_TAG, message);
+                break;
+            default:
+                Log.d(JAVASCRIPT_TAG, message);
+        }
+    }
+
+    public void logInContainer(String message) {
+        logInContainer(message, Log.DEBUG);
+    }
+}
