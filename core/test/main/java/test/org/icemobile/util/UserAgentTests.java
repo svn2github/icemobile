2f4eab7590f3b6e8c24934cb9cb90f8f4ab39f96
philip.breau <philip.breau@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-10-24 14:57:28 +0000

    add more user agent data to tests
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@31719 1de00972-ac04-4911-a46b-06793c8d78d9

--- core/test/main/java/test/org/icemobile/util/UserAgentTests.java
+++ core/test/main/java/test/org/icemobile/util/UserAgentTests.java
@@ -1,19 +1,18 @@
 package test.org.icemobile.util;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 import org.icemobile.util.UserAgentInfo;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 
 public class UserAgentTests  {
     
