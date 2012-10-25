3861825cabdde0c9661717789fcc7633d5c20955
judy.guglielmin <judy.guglielmin@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-10-25 19:20:07 +0000

    MOBI-440 cleaned up javascript as eventlisteners only need to be added for resize (just executing excess javascript).
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@31765 1de00972-ac04-4911-a46b-06793c8d78d9

--- jsf/components/component/resources/org.icefaces.component.splitpane/splitpane.js
+++ jsf/components/component/resources/org.icefaces.component.splitpane/splitpane.js
@@ -15,34 +15,10 @@ ice.mobi.splitpane = {
         }
     },
     resizeHt: function(clientId) {
-         // should put if the node is not found in the document, remove??
         if (this.panels[clientId])
             this.panels[clientId].resize(clientId);
         else {
             this.panels[clientId].unload(clientid);
-        }
-    },
-    resizeElementHeight: function(elId){
-        var height = 0;
-        var leftNode = document.getElementById(elId+"_left");
-        var rtNode = document.getElementById(elId+"_right");
-        var splt  = document.getElementById(elId+"_splt");
-        var body = window.document.body || null;
-        if (body ==null) return;
-        if (leftNode && rtNode){
-            if (window.innerHeight) {
-                height = window.innerHeight;
-            } else if (body.parentElement.clientHeight) {
-                height = body.parentElement.clientHeight;
-            } else if (body) {
-                if (body.clientHeight) {
-                    height = body.clientHeight;
-                }
-            }
-            if (height > 0){
-                leftNode.style.height = ((height - leftNode.offsetTop) + "px");
-                rtNode.style.height = ((height - rtNode.offsetTop) + "px");
-            }
         }
     },
     Scrollable: function Scrollable(clientId, cfgIn) {
@@ -57,28 +33,45 @@ ice.mobi.splitpane = {
                 leftNode.style.width=width + "%";
                 rightNode.style.width=(100-width) + "%";
             }
-            var scrollEvent = 'ontouchstart' in window ? "touchmove" : "scroll";
-            var supportsOrientationChange = "onorientationchange" in window,
-                orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
-            if (window.addEventListener) {
-                    window.addEventListener(orientationEvent, resizeCall, false);
-                    window.addEventListener('resize', resizeCall, false);
-                } else { // older ie event listener
-                    window.attachEvent(orientationEvent, resizeCall, false);
-                    window.attachEvent("resize", resizeCall, false);
-            }
-                   }
+        }
+    /*    var supportsOrientationChange = "onorientationchange" in window,
+            orientationEvent = supportsOrientationChange ? "orientationchange" : "resize"; */
+        if (window.addEventListener) {
+            //    window.addEventListener(orientationEvent, resizeCall, false);
+                window.addEventListener('resize', resizeCall, false);
+            } else { // older ie event listener
+             //   window.attachEvent(orientationEvent, resizeCall, false);
+                window.attachEvent("resize", resizeCall, false);
+        }
         return {
-           resize: function(clientId){
-                ice.mobi.splitpane.resizeElementHeight(clientId);
+           resize: function(elId){
+                var height = 0;
+                var leftNode = document.getElementById(elId+"_left");
+                var rtNode = document.getElementById(elId+"_right");
+                var splt  = document.getElementById(elId+"_splt");
+                var body = window.document.body || null;
+                if (body ==null) return;
+                if (leftNode && rtNode){
+                    if (window.innerHeight) {
+                        height = window.innerHeight;
+                    } else if (body.parentElement.clientHeight) {
+                        height = body.parentElement.clientHeight;
+                    } else if (body) {
+                        if (body.clientHeight) {
+                            height = body.clientHeight;
+                        }
+                    }
+                    if (height > 0){
+                        leftNode.style.height = ((height - leftNode.offsetTop) + "px");
+                        rtNode.style.height = ((height - rtNode.offsetTop) + "px");
+                    }
+                }
            },
-           unload: function(clientId){
+           unload: function(){
                //remove listeners and set object back to empty
                if (window.addEventListener) {
-                   window.removeEventListener(orientationEvent, resizeCall, false);
                    window.removeEventListener("resize", resizeCall, false);
                }else {
-                   window.detachEvent(orientationEvent, resizeCall, false);
                    window.detachEvent("resize", resizeCall, false);
                }
            }
