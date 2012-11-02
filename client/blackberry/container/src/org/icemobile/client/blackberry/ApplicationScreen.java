c337ec8d076dab6b9c09f9e161851b64c2572292
greg.dick <greg.dick@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-11-02 23:33:19 +0000

    mobi-298 Added memory diagnostic menu item
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@31955 1de00972-ac04-4911-a46b-06793c8d78d9

--- client/blackberry/container/src/org/icemobile/client/blackberry/ApplicationScreen.java
+++ client/blackberry/container/src/org/icemobile/client/blackberry/ApplicationScreen.java
@@ -12,11 +12,12 @@
  * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  * express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
- */
+ */ 
 package org.icemobile.client.blackberry;
 
 import org.icemobile.client.blackberry.menu.BackMenuItem;
 import org.icemobile.client.blackberry.menu.HistorySubMenuItem;
+import org.icemobile.client.blackberry.menu.LogMemoryMenuItem;
 import org.icemobile.client.blackberry.menu.ReloadCurrentMenuItem;
 import org.icemobile.client.blackberry.menu.ReloadHomeMenuItem;
 import org.icemobile.client.blackberry.menu.ResetAuthorizationMenuItem;
@@ -30,113 +31,120 @@ import net.rim.device.api.ui.component.Menu;
 import net.rim.device.api.ui.container.MainScreen;
 import net.rim.device.api.ui.menu.SubMenu;
 
-/**
- * A class extending the MainScreen class, which provides default standard
- * behavior for BlackBerry GUI applications.
- */
-public final class ApplicationScreen extends MainScreen {
-
-    private ContainerController mController;
-    // Blackberry menu subclasses for the primary screen. Each subscreen will define
-    // context specific options within the Screen subclasses themselves.
-    private MenuItem mReloadMenu;        // reload Home page
-    private MenuItem mReloadCurrentMenu; // reload current page
-    private MenuItem mURLMenu;           // allow user to manually define HOME_URL locally
-    // without going into options system.
-    private MenuItem mResetAuthCacheMenu; // Reset the authorization cache
-
-
-    //     private MenuItem mResetPushMenu;
-    private MenuItem mBackMenu;
+ /**
+  * A class extending the MainScreen class, which provides default standard
+  * behavior for BlackBerry GUI applications.
+  */
+ public final class ApplicationScreen extends MainScreen {
+     
+     private ContainerController mController; 
+     // Blackberry menu subclasses for the primary screen. Each subscreen will define 
+     // context specific options within the Screen subclasses themselves. 
+     private MenuItem mReloadMenu;        // reload Home page
+     private MenuItem mReloadCurrentMenu; // reload current page 
+     private MenuItem mURLMenu;           // allow user to manually define HOME_URL locally
+                                          // without going into options system.
+     private MenuItem mResetAuthCacheMenu; // Reset the authorization cache
+     private MenuItem mLogMemMenu;
+     
+     
+//     private MenuItem mResetPushMenu;
+     private MenuItem mBackMenu; 
 //     private MenuItem mTestJavascriptMenu;
+     
+     
+     private HistorySubMenuItem[] mHistorySubs;
+     
+     /**
+      * Creates a new MyScreen object
+      */
+     public ApplicationScreen( ContainerController controller ) {
+         super(HORIZONTAL_SCROLL | VERTICAL_SCROLL); 
+//         super();
+         mController = controller; 
+        
+         mHistorySubs = new HistorySubMenuItem[ ICEmobileContainer.HISTORY_SIZE ];
+         loadMenus();
+     }
 
+     // 
 
-    private HistorySubMenuItem[] mHistorySubs;
+     public boolean onClose() { 
+         UiApplication.getUiApplication().requestBackground(); 
+         return false; 
+     }
 
-    /**
-     * Creates a new MyScreen object
-     */
-    public ApplicationScreen(ContainerController controller) {
-        super(HORIZONTAL_SCROLL | VERTICAL_SCROLL);
-        mController = controller;
+     public void close() { 
 
-        mHistorySubs = new HistorySubMenuItem[ICEmobileContainer.HISTORY_SIZE];
-        loadMenus();
-    }
+     }
+     
+     /**
+      * Define the options subclasses for the MainScreen
+      */
+     private void loadMenus() {
 
-
-    public boolean onClose() {
-        UiApplication.getUiApplication().requestBackground();
-        return false;
-    }
-
-    public void close() {
-
-    }
-
-    /**
-     * Define the options subclasses for the MainScreen
-     */
-    private void loadMenus() {
-
-        mReloadMenu = new ReloadHomeMenuItem(mController);
-        mReloadCurrentMenu = new ReloadCurrentMenuItem(mController);
+         mReloadMenu = new ReloadHomeMenuItem(mController);         
+         mReloadCurrentMenu = new ReloadCurrentMenuItem(mController);
 //         mResetAudioMenu = new ResetAudioMenuItem( mContainer); 
 //         mResetPushMenu = new ResetPushMenuItem(mController);         
-        mBackMenu = new BackMenuItem(mController);
-        mURLMenu = new URLEntryMenu(mController);
+         mBackMenu = new BackMenuItem(mController); 
+         mURLMenu = new URLEntryMenu(mController);
 //         mTestJavascriptMenu = new TestJavascriptMenuItem(mContainer);
-        mResetAuthCacheMenu = new ResetAuthorizationMenuItem(mController);
-
-
-    }
-
-    protected void makeMenu(Menu menu, int instance) {
-
-        menu.add(mReloadMenu);
-        menu.add(mReloadCurrentMenu);
-        menu.add(mBackMenu);
+         mResetAuthCacheMenu = new ResetAuthorizationMenuItem(mController); 
+         mLogMemMenu = new LogMemoryMenuItem(mController);
+         
+        
+         
+     }
+     
+     protected void makeMenu ( Menu menu, int instance ) {     
+    
+    	 menu.add ( mReloadMenu ); 
+    	 menu.add ( mReloadCurrentMenu);
+    	 menu.add( mBackMenu );
 //         menu.add (mResetAudioMenu);
 //    	 menu.add (mRerunScriptMenu);
-        menu.add(mURLMenu);
-        menu.add(mResetAuthCacheMenu);
+    	 menu.add(mURLMenu);
+    	 menu.add(mResetAuthCacheMenu);
+    	 menu.add(mLogMemMenu);
 //    	 menu.add(mTestJavascriptMenu);
 //    	 menu.add(mResetPushMenu);  
+    	 
+    	 
+    	 SubMenu urlSubmenu = new SubMenu(null, "URL History", 5, 3);
+    	 if (mHistorySubs[0] == null) { 
+    		 initSubmenuStructure();
+    	 }
+    	 for (int idx = 0; idx < mHistorySubs.length; idx ++ ) { 
+    		 urlSubmenu.add( mHistorySubs[idx] );
+    	 }
 
+         menu.add( urlSubmenu );
+     }
 
-        SubMenu urlSubmenu = new SubMenu(null, "URL History", 5, 3);
-        if (mHistorySubs[0] == null) {
-            initSubmenuStructure();
-        }
-        for (int idx = 0; idx < mHistorySubs.length; idx++) {
-            urlSubmenu.add(mHistorySubs[idx]);
-        }
-
-        menu.add(urlSubmenu);
-    }
-
-    private void initSubmenuStructure() {
-        for (int idx = 0; idx < mHistorySubs.length; idx++) {
-            mHistorySubs[idx] = new HistorySubMenuItem(mController, "");
-        }
-    }
-
-    /**
-     * Notification to update the history menus
-     */
-    public void updateHistoryMenus() {
-
-        if (mHistorySubs[0] == null) {
-            initSubmenuStructure();
-        }
-        String[] urlHistory = mController.getHistoryManager().getHistoryLocations();
-        for (int idx = 0; idx < mHistorySubs.length; idx++) {
-            if (idx >= urlHistory.length || urlHistory[idx] == null) {
-                break;
-            }
-            // These can be null on initial page load when history is added
-            // before the menu has been
-            mHistorySubs[idx].setLabel(urlHistory[idx]);
-        }
-    }
-}
+	private void initSubmenuStructure() {
+		for (int idx = 0; idx < mHistorySubs.length; idx ++ ) { 
+    		 mHistorySubs[idx] = new HistorySubMenuItem( mController, "" );  
+    	 }
+	} 
+    	 
+    	 
+     /**
+      * Notification to update the history menus
+      */
+     public void updateHistoryMenus() { 
+    	 
+    	 if (mHistorySubs[0] == null) { 
+    		 initSubmenuStructure();
+    	 }
+    	 String[] urlHistory = mController.getHistoryManager().getHistoryLocations();
+    	 for (int idx = 0; idx < mHistorySubs.length; idx ++ ) { 
+    		 if (idx >= urlHistory.length || urlHistory[idx] == null) { 
+    			 break;
+    		 }
+    		 // These can be null on initial page load when history is added 
+    		 // before the menu has been 
+    		 mHistorySubs[idx].setLabel( urlHistory[idx] );  
+    	 } 
+     }
+ }
