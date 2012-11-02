4fe710aab74ad57c8578f038d620dff436cc9a81
greg.dick <greg.dick@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-11-02 23:15:25 +0000

    MOBI-238 Added trim() statement to clean up detected tokens
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@31950 1de00972-ac04-4911-a46b-06793c8d78d9

--- client/blackberry/container/src/org/icemobile/client/blackberry/utils/UploadUtilities.java
+++ client/blackberry/container/src/org/icemobile/client/blackberry/utils/UploadUtilities.java
@@ -12,7 +12,7 @@
  * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  * express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
- */
+ */ 
 package org.icemobile.client.blackberry.utils;
 
 import java.io.ByteArrayOutputStream;
@@ -31,17 +31,15 @@ public class UploadUtilities {
 
     /**
      * Get the name value pairs from an URLEncoded string like aaa=bbb&ccc=ddd
-     *
-     * @param data   The string to parse.
-     * @param delim1 The first delimiter, typically "="
-     * @param delim2 The second delimiter, typically "&"
+     * @param data The string to parse. 
+     * @param delim1 The first delimiter, typically "=" 
+     * @param delim2 The second delimiter, typically "&" 
      * @return Array of NameValuePair objects from parsed string
      */
-    public static NameValuePair[] getNameValuePairs(String data, String delim1,
-                                                    String delim2) {
+    public static  NameValuePair[] getNameValuePairs(String data, String delim1, String delim2) {
 
         NameValuePair[] returnVal = new NameValuePair[0];
-        if (!(data.indexOf(delim1) > -1)) {
+        if (!(data.indexOf(delim1) > -1) ) {
             return returnVal;
         } else {
 
@@ -49,137 +47,131 @@ public class UploadUtilities {
             String nameValuePairs[] = split(data, delim2);
 
             NameValuePair[] res = new NameValuePair[nameValuePairs.length];
-            for (int i = 0; i < nameValuePairs.length; i++) {
+            for (int i=0; i<nameValuePairs.length; i++) {
 
-                String[] nameValue = split(nameValuePairs[i], delim1);
+                String [] nameValue = split( nameValuePairs[i], delim1);
 
                 if (nameValue.length == 2) {
-                    res[i] = new NameValuePair(nameValue[0], nameValue[1]);
+                    res[i] = new NameValuePair(nameValue[0],nameValue[1]);
                 } else {
-                    res[i] = new NameValuePair(nameValue[0], "");
+                    res[i] = new NameValuePair(nameValue[0],"");
                 }
-            }
+            }		
             return res;
         }
     }
-
     /**
      * Split a string into a number of tokens based on a single delimiter
-     *
-     * @param str   String to parse
+     * @param str String to parse 
      * @param delim The delimiter
-     * @return Array of Strings
+     * @return Array of Strings 
      */
-    public static String[] split(String str, String delim) {
+    public static String[] split (String str, String delim) { 
 
         Vector v = new Vector();
-        int spos = 0;
+        int spos = 0; 
 
-        int epos = str.indexOf(delim);
+        int epos = str.indexOf( delim ); 
         while (true) {
             // Catch the end case. Might be zero length, or might have to catch case when str= "aaa="
-            if (epos == -1) {
-                if (str.length() > spos) {
-                    v.addElement(str.substring(spos));
+            if (epos == -1) { 
+                if (str.length() > spos) { 
+                    v.addElement(str.substring(spos).trim());
                 }
                 break;
             }
 
-            v.addElement(str.substring(spos, epos).trim());
+            v.addElement(  str.substring(spos, epos).trim() );			
 
             spos = epos + delim.length();
-            epos = str.indexOf(delim, spos + 1);
+            epos = str.indexOf( delim, spos + 1);
 
-        }
-        String[] returnVal = new String[v.size()];
+        }		
+        String[] returnVal = new String[ v.size() ]; 
         v.copyInto(returnVal);
         return returnVal;
     }
-
+    
     /**
-     * Get the set of name value pairs from an URLEncoded string like aaa=bbb&ccc=ddd, but
-     * in handy Hashtable form. NameValuePairs are added to the table using the
+     * Get the set of name value pairs from an URLEncoded string like aaa=bbb&ccc=ddd, but 
+     * in handy Hashtable form. NameValuePairs are added to the table using the 
      * name property
-     *
-     * @param data   The string to parse.
-     * @param delim1 The first delimiter, typically "="
-     * @param delim2 The second delimiter, typically "&"
+     * 
+     * @param data The string to parse. 
+     * @param delim1 The first delimiter, typically "=" 
+     * @param delim2 The second delimiter, typically "&" 
      * @return Hashtable of NameValuePair objects from parsed string, keyed by pair name
      */
-    public static Hashtable getNameValuePairHash(String data, String delim1,
-                                                 String delim2) {
+    public static Hashtable getNameValuePairHash(String data, String delim1, String delim2) {
         NameValuePair[] array = getNameValuePairs(data, delim1, delim2);
         Hashtable returnVal = new Hashtable();
-        NameValuePair item;
-        for (int idx = 0; idx < array.length; idx++) {
+        NameValuePair item; 
+        for (int idx = 0; idx < array.length; idx ++ ) { 
             item = array[idx];
-            returnVal.put(item.getName(), item);
+            returnVal.put( item.getName(), item);
         }
-        return returnVal;
+        return returnVal;        
     }
 
     /**
-     * BlackBerry doesn't have a String.replace(str, str) function?
-     *
-     * @param original    original String
-     * @param toReplace   String to replace
+     * BlackBerry doesn't have a String.replace(str, str) function? 
+     * @param original original String 
+     * @param toReplace String to replace
      * @param replaceWith String to replace with
      * @return Amended string
      */
-    public static String replace(String original, String toReplace,
-                                 char[] replaceWith) {
+    public static String replace (String original, String toReplace, char[] replaceWith) {
 
         StringBuffer retVal = new StringBuffer();
         int epos = original.indexOf(toReplace);
         int spos = 0;
 
         while (epos > -1) {
-            retVal.append(original.substring(spos, epos));
-            for (int idx = 0; idx < replaceWith.length; idx++) {
-                retVal.append(replaceWith[idx]);
+            retVal.append( original.substring(spos, epos));
+            for (int idx = 0; idx < replaceWith.length; idx ++) {
+                retVal.append(replaceWith[idx] );
             }
             spos = epos + toReplace.length();
-            epos = original.indexOf(toReplace, epos + toReplace.length());
+            epos = original.indexOf(toReplace, epos + toReplace.length() );
         }
         if (original.length() > spos) {
-            retVal.append(original.substring(spos));
+            retVal.append( original.substring(spos ));
         }
         return retVal.toString();
     }
 
     /**
-     * Read a local file
-     *
+     * Read a local file 
      * @param filename
      * @return The contents of said file
      */
-    private String readFile(String filename) throws IOException {
+    private String readFile( String filename ) throws IOException { 
 
-        InputStream is = null;
+        InputStream is = null; 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         FileConnection fconn = null;
 
-        try {
+        try { 
             fconn = (FileConnection) Connector.open(filename);
             if (fconn.exists()) {
-                is = fconn.openInputStream();
+                is = fconn.openInputStream(); 
                 byte buf[] = new byte[1024];
-                int len;
-                while ((len = is.read(buf)) != -1) {
-                    outputStream.write(buf, 0, len);
-                }
+                int len; 
+                while ((len = is.read(buf)) != -1) { 
+                    outputStream.write(buf, 0, len); 
+                } 
 
             }
-        } catch (IOException ioe) {
-            Logger.ERROR("readFile - Exception opening file: " + ioe + ", path of file: " +
-                             fconn.getPath());
+        } catch (IOException ioe) { 
+        	Logger.ERROR("readFile - Exception opening file: " + ioe + ", path of file: " +
+                    fconn.getPath());
 
 
-        } finally {
-            if (is != null) {
-                is.close();
+        } finally { 
+            if (is != null) { 
+                is.close(); 
             }
-            if (outputStream != null) {
+            if (outputStream != null) { 
                 outputStream.close();
             }
         }
