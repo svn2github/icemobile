abe27a06a3fbae81834d6ae66e369b01c8faad1a
greg.dick <greg.dick@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-11-28 19:59:27 +0000

    MOBI-666 Removed pattern property
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@32469 1de00972-ac04-4911-a46b-06793c8d78d9

--- core/src/main/java/org/icemobile/component/IContactList.java
+++ core/src/main/java/org/icemobile/component/IContactList.java
@@ -4,8 +4,6 @@ public interface IContactList extends IMobiComponent{
     
     public String getButtonLabel();
     public void setButtonLabel(String label);
-    public String getPattern();
-    public void setPattern(String pattern);
     public String getFields();
     public void setFields(String fields);
     public String getScript(String id, boolean isSX);
