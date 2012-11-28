7358ad8ebd4655d0dd75aa9b13e0b18549bd4a51
greg.dick <greg.dick@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-11-28 06:26:07 +0000

    MOBI-666 Added annotated method for retrieving qr code image directly
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@32446 1de00972-ac04-4911-a46b-06793c8d78d9

--- samples/icemobilespring/src/main/java/org/icemobile/samples/springbasic/EchoSessionController.java
+++ samples/icemobilespring/src/main/java/org/icemobile/samples/springbasic/EchoSessionController.java
@@ -1,6 +1,9 @@
 
 package org.icemobile.samples.springbasic;
 
+import org.icemobile.zxing.qrcode.QRCodeWriter;
+import org.icemobile.zxing.common.BitMatrix;
+import org.icemobile.zxing.BarcodeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -11,7 +14,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Session Controller for echoing simple input pages
@@ -20,6 +30,11 @@ import javax.servlet.http.HttpServletRequest;
 @SessionAttributes("QRScanBean")
 public class EchoSessionController {
 
+private static Logger LOG = Logger.getLogger(EchoSessionController.class.getName());
+
+     private String PLAINTEXT_DEFAULT = "Is there in truth, no beauty?";
+
+
     @ModelAttribute("QRScanBean")
     public QRScanBean initQRScanBean() {
         return new QRScanBean();
@@ -27,14 +42,64 @@ public class EchoSessionController {
 
     @RequestMapping(value = "/qrscan")
     public void doRequest(
-            @ModelAttribute("QRScanBean") QRScanBean model) {
+        @ModelAttribute("QRScanBean") QRScanBean model) {
     }
-    
+
+    @RequestMapping(value = "/qrscan/theImage")
+    public void doImageRequest( HttpServletResponse response,
+                                @ModelAttribute("QRScanBean") QRScanBean model) {
+
+        String plaintext = model.getPlaintextOne();
+        if (plaintext == null || "".equals(plaintext)) {
+            plaintext = PLAINTEXT_DEFAULT;
+        }
+        OutputStream out = null;
+        try {
+            out = response.getOutputStream();
+            writeQRToStream(plaintext, out);
+
+        } catch (IOException ioe) {
+            LOG.severe("IOException writing QRCode to stream");
+        }
+    }
+
     @ModelAttribute
     public void ajaxAttribute(WebRequest request, Model model) {
         model.addAttribute("ajaxRequest", AjaxUtils.isAjaxRequest(request));
     }
 
+    private void writeQRToStream(String data, OutputStream out)  {
+        try {
+            int bigEnough = 80;
+            QRCodeWriter writer = new QRCodeWriter();
+            BitMatrix matrix = writer.encode(data,
+                                             BarcodeFormat.QR_CODE, bigEnough,
+                                             bigEnough, null);
 
+            int width = 160, height = 160;
+
+            BufferedImage bi = new BufferedImage(
+                width, height, BufferedImage.TYPE_INT_ARGB);
+
+            Graphics2D ig2 = bi.createGraphics();
+            ig2.setColor(Color.WHITE);
+            ig2.fillRect(0, 0, width, height);
+            ig2.setColor(Color.BLACK);
+            int d = width / bigEnough;
+            int bitWidth = matrix.getWidth();
+            int bitHeight = matrix.getHeight();
+            for (int i = 0; i < bitHeight; i++)  {
+                for (int j = 0; j < bitWidth; j++)  {
+                    boolean isSet = matrix.get(j, i);
+                    if (isSet)  {
+                        ig2.fillRect(d * j, d * i, d, d);
+                    }                 }
+            }
+            ImageIO.write(bi, "PNG", out);
+
+        } catch (Exception e)  {
+            LOG.log(Level.WARNING, "Error create QR Code image", e);
+        }
+    }
 }
 
