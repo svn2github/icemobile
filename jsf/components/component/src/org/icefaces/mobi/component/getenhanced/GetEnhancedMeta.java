b1726d756a27fa564f16ecaaaa37cc4736b56489
philip.breau <philip.breau@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-12-07 19:06:23 +0000

    MOBI-580 TLD Doc Update getEnhanced update add TLDConstants.java
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@32659 1de00972-ac04-4911-a46b-06793c8d78d9

--- jsf/components/component/src/org/icefaces/mobi/component/getenhanced/GetEnhancedMeta.java
+++ jsf/components/component/src/org/icefaces/mobi/component/getenhanced/GetEnhancedMeta.java
@@ -23,6 +23,7 @@ import javax.faces.application.ResourceDependency;
 import org.icefaces.ace.meta.annotation.Component;
 import org.icefaces.ace.meta.annotation.Property;
 import org.icefaces.ace.meta.baseMeta.UIComponentBaseMeta;
+import org.icefaces.mobi.utils.TLDConstants;
 
 @Component(
         tagName = "getEnhanced",
@@ -34,7 +35,9 @@ import org.icefaces.ace.meta.baseMeta.UIComponentBaseMeta;
         extendsClass = "javax.faces.component.UIComponentBase",
         componentFamily = "org.icefaces.GetEnhanced",
         tlddoc = "This mobility component renders a warning when the ICEmobile container" +
-        		" is not detected, and will render a device-dependent link to the download."
+        		" is not detected, and will render a device-dependent link to the ICEmobile container download. This component should" +
+        		" be used in association with other ICEmobile container-dependent components, such as the camera or camcorder, which" +
+        		" require the ICEmobile container for optimum use."
 )
 @ResourceDependencies({
         @ResourceDependency(library = "org.icefaces.component.util", name = "component.js")
@@ -42,28 +45,26 @@ import org.icefaces.ace.meta.baseMeta.UIComponentBaseMeta;
 public class GetEnhancedMeta extends UIComponentBaseMeta {
 
 	@Property(defaultValue = "false", 
-            tlddoc = "When disabled, geolocation is not activated")
+            tlddoc = TLDConstants.DISABLED)
     private boolean disabled;
 
-    @Property(tlddoc = "style will be rendered on the root element of this " +
-            "component.")
+    @Property(tlddoc = TLDConstants.STYLE)
     private String style;
 
-    @Property(tlddoc = "style class will be rendered on the root element of " +
-            "this component.")
+    @Property(tlddoc = TLDConstants.STYLECLASS)
     private String styleClass;
     
     @Property(defaultValue = "true",
     		tlddoc = "When set to false, the ICEmobile download link will not be displayed.")
     private boolean includeLink;
     
-    @Property(tlddoc="The custom download message link for an Android device")
+    @Property(tlddoc="The custom download message link for an Android device.")
     private String androidMsg;
     
-    @Property(tlddoc="The custom download message link for an iOS device")
+    @Property(tlddoc="The custom download message link for an iOS device.")
     private String iosMsg;
     
-    @Property(tlddoc="The custom download message link for an BlackBerry device")
+    @Property(tlddoc="The custom download message link for an BlackBerry device.")
     private String blackberryMsg;
     
     
---
+++ jsf/components/component/src/org/icefaces/mobi/utils/TLDConstants.java
@@ -0,0 +1,9 @@
+package org.icefaces.mobi.utils;
+
+public class TLDConstants {
+    
+    public static final String STYLE = "The style attribute sets the CSS style definition to be applied to this component when it is rendered.";
+    public static final String STYLECLASS = "The styleClass attribute sets the CSS class to apply to this component when it is rendered.";
+    public static final String DISABLED = "The disabled attribute is a boolean flag that when set to true indicates that this component should not receive focus or be included in a form submit.";
+
+}
