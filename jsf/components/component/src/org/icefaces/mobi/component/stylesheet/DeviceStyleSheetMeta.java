278ff46b91a75807caa36988f76a8344a1d41df3
judy.guglielmin <judy.guglielmin@1de00972-ac04-4911-a46b-06793c8d78d9>
2012-12-10 20:48:17 +0000

    MOBI-580 TLD docs and MOBI-569 remove unused href attribute
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@32706 1de00972-ac04-4911-a46b-06793c8d78d9

--- jsf/components/component/src/org/icefaces/mobi/component/stylesheet/DeviceStyleSheetMeta.java
+++ jsf/components/component/src/org/icefaces/mobi/component/stylesheet/DeviceStyleSheetMeta.java
@@ -31,9 +31,24 @@ import javax.faces.application.ResourceDependency;
         rendererType = "org.icefaces.DeviceStyleSheetRenderer",
         extendsClass = "javax.faces.component.UIComponentBase",
         componentFamily = "org.icefaces.DeviceStyleSheet",
-        tlddoc = "This mobility component loads an outputStyleSheet in the head of a jsf view page " +
-                "while determining which device is to be supported and delivering the correct stylesheet " +
-                "based on the device type"
+        tlddoc = "This mobility component loads an outputStyleSheet in the head of a jsf view page based " +
+                "upon a detected device/desktop or a user-specified theme. " +
+                "If the JSF content parameter of Project Stage evaluates to \"Production\" the " +
+                "compressed css file is loaded into the head.  " +
+                "The component has three modes in which it executes. \n"+
+                "1.) no attributes - then component tries to detect a mobile device "+
+                "in from the user-agent.  If a mobile device is discovered, then "+
+                "it will fall into three possible matches, iphone, ipad,  android and "+
+                "blackberry.  If the mobile device is not not know then ipad as default "+
+                "is loaded. Library is always assumed to be DEFAULT_LIBRARY. \n"+
+                "2.) name attribute - component will default to using a library name  "+
+                "of DEFAULT_LIBRARY.  The name attribute specifies one of the "+
+                "possible device themes; iphone.css, android.css or bberry.css "+
+                "An Error will result if named resource could not be resolved.\n" +
+                "3.) name and libraries attributes. - component will use the library "+
+                "and name specified by the user.  Component is fully manual in this  "+
+                "mode. Error will result if name and library can not generate a "+
+                "value resource. "
 )
 
 
@@ -42,18 +57,18 @@ import javax.faces.application.ResourceDependency;
 })
 public class DeviceStyleSheetMeta {
 
-    @Property(tlddoc = "href for css file to be loaded.  The default for iPhone will be default_iPhone")
-    private String href;
+ /*   @Property(tlddoc = "href for css file to be loaded.  The default for iPhone will be default_iPhone")
+    private String href; */
 
-    @Property(tlddoc = "name is one of the attributes used for the h component outputStyleSheet")
+    @Property(tlddoc = "The name reference for the stylesheet as a JSF resource.")
     private String name;
 
-    @Property(tlddoc = "library is also used by h comp outputStyleSheet")
+    @Property(tlddoc = "THe library reference for the stylesheet as a  JSF resource.")
     private String library;
 
-    @Property(tlddoc = "CSS media type, screen, print and others. ")
+    @Property(tlddoc = "The CSS media type, screen, print and others. ")
     private String media;
 
-    @Property(tlddoc = "View size, small or large ")
+    @Property(tlddoc = "View size, small or large as per  ")
     private String view;
 }
