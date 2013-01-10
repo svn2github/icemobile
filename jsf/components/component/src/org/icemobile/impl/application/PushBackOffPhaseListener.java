b9d0cea3c3a619a99aade38453cf460096c4a04a
ted.goddard <ted.goddard@1de00972-ac04-4911-a46b-06793c8d78d9>
2013-01-10 01:07:10 +0000

    make use of pushContext.backOff to improve page updates on iOS which required icepush.jar from trunk revision 33015 and icefaces.jar from trunk revision 33014 (PUSH-205)
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@33016 1de00972-ac04-4911-a46b-06793c8d78d9

---
+++ jsf/components/component/src/org/icemobile/impl/application/PushBackOffPhaseListener.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2004-2012 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icemobile.impl.application;
+
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.Cookie;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+import org.icepush.PushContext;
+
+public class PushBackOffPhaseListener implements PhaseListener {
+
+    public void afterPhase(PhaseEvent phaseEvent)  {
+    }
+
+    public void beforePhase(PhaseEvent phaseEvent)  {
+        FacesContext facesContext = phaseEvent.getFacesContext();
+        ExternalContext externalContext = facesContext.getExternalContext();
+        if (PhaseId.RENDER_RESPONSE == phaseEvent.getPhaseId()) {
+            String browserID = getBrowserID(facesContext);
+            if (null != browserID)  {
+                PushContext pushContext = PushContext.getInstance(
+                    (ServletContext) externalContext.getContext());
+                pushContext.backOff(browserID, 300);
+            }
+        }
+    }
+
+    private static String getBrowserID(FacesContext facesContext)  {
+        ExternalContext externalContext = facesContext.getExternalContext();
+        Map cookies = externalContext.getRequestCookieMap();
+        if (cookies != null) {
+            Cookie cookie = (Cookie) cookies.get("ice.push.browser");
+            if (null != cookie)  {
+                return cookie.getValue();
+            }
+        }
+        return null;
+    }
+
+    public PhaseId getPhaseId() {
+        return PhaseId.RENDER_RESPONSE;
+    }
+
+}
