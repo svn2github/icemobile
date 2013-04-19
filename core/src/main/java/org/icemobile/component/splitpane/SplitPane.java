4524de9554b4f111f65c0e82b1cdad11a3d8f7b9
mark.collette <mark.collette@1de00972-ac04-4911-a46b-06793c8d78d9>
2013-04-19 22:42:48 +0000

    MOBI-697 : Migrate distinct jsf/jsp components and tags to common core generation
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@34477 1de00972-ac04-4911-a46b-06793c8d78d9

---
+++ core/src/main/java/org/icemobile/component/splitpane/SplitPane.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2004-2013 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icefaces.mobi.component.splitpane;
+
+import org.icefaces.mobi.utils.MobiJSFUtils;
+import org.icemobile.component.ISplitPane;
+import org.icemobile.util.ClientDescriptor;
+
+
+public class SplitPane extends SplitPaneBase implements ISplitPane{
+
+    public static final String SPLITPANE_BASE = "mobi-splitpane" ;
+    public static final String SPLITPANE_NONSCROLL = "mobi-splitpane-nonScrollable";
+    public static final String SPLITPANE_SCROLLABLE = "mobi-splitpane-scrollable";
+    public static final String SPLITPANE_DIVIDER = "mobi-splitpane-divider";
+
+
+   // facet names that define the optionally defined layouts
+    public static final String LEFT_FACET = "left";
+    public static final String RIGHT_FACET = "right";
+
+    public ClientDescriptor getClient() {
+         return MobiJSFUtils.getClientDescriptor();
+    }
+
+    public boolean isDisabled(){
+        return false;
+    }
+    public void setDisabled(boolean anything){
+        //na
+    }
+}
