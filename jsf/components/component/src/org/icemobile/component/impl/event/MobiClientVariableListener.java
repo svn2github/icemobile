8f271d492c9b96b7402c4908b7737a76d9c75eaf
philip.breau <philip.breau@1de00972-ac04-4911-a46b-06793c8d78d9>
2013-04-29 16:23:23 +0000

    MOBI-615 ICEmobile implicit variables - add listener to inject #{mobiClient} into view scope
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@34576 1de00972-ac04-4911-a46b-06793c8d78d9

---
+++ jsf/components/component/src/org/icemobile/component/impl/event/MobiClientVariableListener.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2004-2013 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.icemobile.component.impl.event;
+
+import java.util.Map;
+
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.SystemEvent;
+import javax.faces.event.SystemEventListener;
+
+import org.icefaces.mobi.utils.MobiJSFUtils;
+
+public class MobiClientVariableListener implements SystemEventListener{
+    
+    private static final String KEY = "mobiClient";
+
+    @Override
+    public boolean isListenerForSource(Object source) {
+        return source instanceof UIViewRoot;
+    }
+
+    @Override
+    public void processEvent(SystemEvent evt) throws AbortProcessingException {
+        FacesContext context = FacesContext.getCurrentInstance();
+        UIViewRoot viewRoot = context.getViewRoot();
+        Map viewMap = viewRoot.getViewMap();
+        viewMap.put(KEY, MobiJSFUtils.getClientDescriptor());
+    }
+
+
+}
