1db6300e0b176f646c18252ca4a0faee938d43dd
ted.goddard <ted.goddard@1de00972-ac04-4911-a46b-06793c8d78d9>
2013-05-03 23:09:24 +0000

    optional ExternalContext that will disable FileEntryResourceHandler (ICE-9200)
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@34715 1de00972-ac04-4911-a46b-06793c8d78d9

---
+++ jsf/components/component/src/org/icemobile/context/ACECompatibleExternalContext.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2004-2013 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icemobile.context;
+
+import javax.faces.context.FacesContext;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.ExternalContextWrapper;
+
+
+public class ACECompatibleExternalContext extends ExternalContextWrapper {
+    ExternalContext wrapped;
+    private static String FILE_ENTRY =
+        "org.icefaces.ace.component.fileentry.FileEntryResourceHandler";
+
+    public ACECompatibleExternalContext(ExternalContext wrapped)  {
+        this.wrapped = wrapped;
+    }
+
+    public ExternalContext getWrapped()  {
+        return wrapped;
+    }
+
+    public String getRequestContentType()  {
+        StackTraceElement[] traces =
+                Thread.currentThread().getStackTrace();
+        for (int i = 0; i < traces.length; i++)  {
+            if ( FILE_ENTRY.equals(traces[i].getClassName()) )  {
+                return "wrappingdisabled";
+            }
+        }
+        return getWrapped().getRequestContentType();
+    }
+}
---
+++ jsf/components/component/src/org/icemobile/context/ACECompatibleExternalContextFactory.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2004-2013 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icemobile.context;
+
+import javax.faces.context.FacesContext;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.ExternalContextFactory;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ACECompatibleExternalContextFactory extends ExternalContextFactory {
+    private static Logger log =
+        Logger.getLogger(
+                ACECompatibleExternalContextFactory.class.getName());
+    ExternalContextFactory wrapped;
+
+    public ACECompatibleExternalContextFactory(
+            ExternalContextFactory wrapped)  {
+        this.wrapped = wrapped;
+        log.info("ACE FileEntry request wrapping disabled.");
+    }
+
+    public ExternalContext getExternalContext(
+            Object context, Object request, Object response)  {
+        return new ACECompatibleExternalContext(
+            wrapped.getExternalContext(context, request, response));
+    }
+
+}
