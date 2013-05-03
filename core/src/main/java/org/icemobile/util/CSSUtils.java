776f017a281c683c36634c3e410230995c0dd204
philip.breau <philip.breau@1de00972-ac04-4911-a46b-06793c8d78d9>
2013-05-03 18:00:50 +0000

    MOBI-82 Theme Roller Support - add base structural theme to better support 3rd party themeroller themes
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@34704 1de00972-ac04-4911-a46b-06793c8d78d9

--- core/src/main/java/org/icemobile/util/CSSUtils.java
+++ core/src/main/java/org/icemobile/util/CSSUtils.java
@@ -58,7 +58,7 @@ public class CSSUtils {
     public static final String STYLECLASS_FIELDCONTAIN = "ui-field-contain";
 
     
-    public enum Theme{ IPAD, IPHONE, BBERRY, ANDROID, HONEYCOMB, ARCHAIC, ANDROID_LIGHT, ANDROID_DARK, BB10, JQM;
+    public enum Theme{ BASE, IPAD, IPHONE, BBERRY, ANDROID, HONEYCOMB, ARCHAIC, ANDROID_LIGHT, ANDROID_DARK, BB10, JQM;
         public String fileName(){
             return this.name().toLowerCase();
         }
