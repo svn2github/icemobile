aaca20630f6a28bea3b230f0ffa5cd80e319a668
philip.breau <philip.breau@1de00972-ac04-4911-a46b-06793c8d78d9>
2013-06-07 17:21:47 +0000

    MOBI-709 - update android link to general icesoft page, where the user can choose between sx or container
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@36052 1de00972-ac04-4911-a46b-06793c8d78d9

--- core/src/main/java/org/icemobile/component/IGetEnhanced.java
+++ core/src/main/java/org/icemobile/component/IGetEnhanced.java
@@ -23,7 +23,7 @@ public interface IGetEnhanced extends IMobiComponent{
     public static final String DOWNLOAD = "Download ICEmobile";
     public static final String ENABLE = "Enable ICEmobile";
     public static final String IOS_LINK = "http://itunes.apple.com/us/app/icemobile-sx/id485908934?mt=8";
-    public static final String ANDROID_LINK = "https://play.google.com/store/apps/details?id=org.icemobile.client.android";
+    public static final String ANDROID_LINK = "https://play.google.com/store/apps/developer?id=ICEsoft+Technologies";
     public static final String BLACKBERRY_LINK = "http://appworld.blackberry.com/webstore/content/59555/";
     public static final String ICEMOBILE_LINK = "http://www.icesoft.org/projects/ICEmobile/containers.jsf";
 
