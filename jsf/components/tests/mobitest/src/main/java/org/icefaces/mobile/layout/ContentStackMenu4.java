4c64da861b9a0dc95f09c11fb71ab91e1382a060
philip.breau <philip.breau@1de00972-ac04-4911-a46b-06793c8d78d9>
2013-07-12 19:33:21 +0000

    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@36671 1de00972-ac04-4911-a46b-06793c8d78d9

---
+++ jsf/components/tests/mobitest/src/main/java/org/icefaces/mobile/layout/ContentStackMenu4.java
@@ -0,0 +1,29 @@
+package org.icefaces.mobile.layout;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.event.ActionEvent;
+
+import org.icefaces.mobi.utils.MobiJSFUtils;
+
+@ManagedBean
+@SessionScoped
+public class ContentStackMenu4 {
+    
+    private String currentPane;
+
+    public String getCurrentPane() {
+        if( currentPane == null || currentPane.length() == 0 ){
+            currentPane = MobiJSFUtils.getClientDescriptor().isHandheldBrowser() ? "menu" : "dataTable";
+        }
+        return currentPane;
+    }
+
+    public void setCurrentPane(String currentPane) {
+        this.currentPane = currentPane;
+    }
+    
+    public void goToMenu(ActionEvent evt){
+        currentPane = "menu";
+        System.out.println("goToMenu()");
+    }
+}
