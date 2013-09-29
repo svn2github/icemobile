cdcae396a84cee8828d378fbed438fd0cfe37509
philip.breau <philip.breau@1de00972-ac04-4911-a46b-06793c8d78d9>
2013-09-29 14:33:47 +0000

    MOBI-890 Tab Set does not calculate client side auto width correctly
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@38303 1de00972-ac04-4911-a46b-06793c8d78d9

--- jsf/components/component/resources/org.icefaces.component.tabset/tabset.js
+++ jsf/components/component/resources/org.icefaces.component.tabset/tabset.js
@@ -61,13 +61,22 @@
             var nodes = root.getElementsByTagName('ul');
             var ul = nodes[0];
             var children = ul.getElementsByTagName('li');
-            var liLngth = children.length;
             var containerWidth = root.clientWidth;        
-            var width = Math.floor(containerWidth/liLngth);
-            var percentageWidth = Math.floor(100/liLngth);
-            for (var i = 0; i < liLngth; i++){
-                children[i].style.width = width+"px";
-                children[i].style.maxWidth = percentageWidth+"%";
+            var width = Math.floor(containerWidth/children.length);
+            var remainingPixels = containerWidth - (children.length*width);
+            var percentageWidth = Math.floor(100/children.length);
+            var remainingPercentage = 100 - (children.length*percentageWidth);
+            
+            for (var i = 0; i < children.length; i++){
+                if( i < (children.length -1 )){
+                    children[i].style.width = width+"px";
+                    children[i].style.maxWidth = percentageWidth+"%";
+                }
+                else{
+                    children[i].style.width = (width+remainingPixels)+"px";
+                    children[i].style.maxWidth = (percentageWidth+remainingPercentage)+"%";
+                }
+                
             }
         }
     }
