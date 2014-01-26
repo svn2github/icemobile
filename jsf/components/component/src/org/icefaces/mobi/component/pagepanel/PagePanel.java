d3861b138dc212f1043ef10538b01b1c96d28c4e
philip.breau <philip.breau@8668f098-c06c-11db-ba21-f49e70c34f74>
2014-01-26 23:13:03 +0000

    MOBI-283 pagePanel fixed positioning unreliable - add overthrow class for Android 2 clients
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@39782 8668f098-c06c-11db-ba21-f49e70c34f74

--- jsf/components/component/src/org/icefaces/mobi/component/pagepanel/PagePanel.java
+++ jsf/components/component/src/org/icefaces/mobi/component/pagepanel/PagePanel.java
@@ -25,7 +25,7 @@ public class PagePanel extends PagePanelBase {
     // base styles for the three page sections
     public static final String HEADER_CLASS = "mobi-pagePanel-header ui-header ";
     public static final String FOOTER_CLASS = "mobi-pagePanel-footer ui-footer ";
-    public static final String BODY_CLASS = "mobi-pagePanel-body ";
+    public static final String BODY_CLASS = "mobi-pagePanel-body overthrow ";
     public static final String CTR_CLASS = "mobi-pagePanel-ctr";
 
     // style classes to remove header footer margins
