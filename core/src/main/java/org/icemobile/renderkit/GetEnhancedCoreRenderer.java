0694983f1c6196231f683fe9602568f50f8f7938
philip.breau <philip.breau@8668f098-c06c-11db-ba21-f49e70c34f74>
2014-01-29 20:46:53 +0000

    MOBI-736 Support for IE8 - getEnhanced layout fixes
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@39840 8668f098-c06c-11db-ba21-f49e70c34f74

--- core/src/main/java/org/icemobile/renderkit/GetEnhancedCoreRenderer.java
+++ core/src/main/java/org/icemobile/renderkit/GetEnhancedCoreRenderer.java
@@ -23,11 +23,11 @@ import static org.icemobile.util.HTML.HREF_ATTR;
 import static org.icemobile.util.HTML.ID_ATTR;
 import static org.icemobile.util.HTML.ONCLICK_ATTR;
 import static org.icemobile.util.HTML.DIV_ELEM;
+import static org.icemobile.util.HTML.SPAN_ELEM;
 import static org.icemobile.util.HTML.STYLE_ATTR;
 
 import java.io.IOException;
 
-import org.icefaces.mobi.utils.HTML;
 import org.icemobile.component.IGetEnhanced;
 import org.icemobile.util.CSSUtils;
 import org.icemobile.util.ClientDescriptor;
@@ -93,9 +93,9 @@ public class GetEnhancedCoreRenderer {
             else{
                 link = IGetEnhanced.ICEMOBILE_LINK;
             }
-            writer.startElement(HTML.SPAN_ELEM);
+            writer.startElement(SPAN_ELEM);
             writer.writeText(msg);
-            writer.endElement(HTML.SPAN_ELEM);
+            writer.endElement(SPAN_ELEM);
 
             if( client.isIOS() || client.isAndroidOS() || client.isBlackBerry10OS() ){
                 writer.startElement(ANCHOR_ELEM, null);
