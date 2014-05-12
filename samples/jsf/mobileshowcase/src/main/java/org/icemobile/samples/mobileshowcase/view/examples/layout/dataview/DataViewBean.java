e99850ccbc35ebb65cc7103761a851d18f13fd4e
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2014-05-12 15:35:17 +0000

    MOBI-1034 Updated copyright header.
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@40996 8668f098-c06c-11db-ba21-f49e70c34f74

--- samples/jsf/mobileshowcase/src/main/java/org/icemobile/samples/mobileshowcase/view/examples/layout/dataview/DataViewBean.java
+++ samples/jsf/mobileshowcase/src/main/java/org/icemobile/samples/mobileshowcase/view/examples/layout/dataview/DataViewBean.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2004-2014 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 package org.icemobile.samples.mobileshowcase.view.examples.layout.dataview;
 
 import org.icemobile.samples.mobileshowcase.view.metadata.annotation.*;
@@ -8,27 +24,6 @@ import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
-/**
- * Copyright 2010-2013 ICEsoft Technologies Canada Corp.
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * <p/>
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * <p/>
- * User: Nils Lundquist
- * Date: 2013-04-01
- * Time: 11:08 AM
- */
 
 @Destination(
         title = "example.layout.dataview.destination.title.short",
