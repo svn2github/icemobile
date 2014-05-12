bf44b1e27341b2f6ca9aefe291c9cc93ff2b1a66
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2014-05-12 15:17:02 +0000

    MOBI-1034 Updated copyright header.
    git-svn-id: http://anonsvn.icesoft.org/repo/icemobile/trunk/icemobile@40993 8668f098-c06c-11db-ba21-f49e70c34f74

--- jsf/components/component/src/org/icefaces/mobi/component/dataview/DataView.java
+++ jsf/components/component/src/org/icefaces/mobi/component/dataview/DataView.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2004-2014 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 package org.icefaces.mobi.component.dataview;
 
 import java.util.ArrayList;
@@ -26,26 +42,6 @@ import org.icemobile.model.DataViewListDataModel;
 import org.icemobile.model.IndexedIterator;
 import org.icemobile.util.ClientDescriptor;
 
-/**
- * Copyright 2010-2013 ICEsoft Technologies Canada Corp.
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * <p/>
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * <p/>
- * User: Nils Lundquist
- * Date: 2013-04-01
- * Time: 10:47 AM
- */
 public class DataView extends DataViewBase implements IDataView, NamingContainer {
     private static Logger logger = Logger.getLogger(DataViewRenderer.class.getName());
     protected static final DataViewDataModel EMPTY_DATA_MODEL = new DataViewListDataModel(Collections.EMPTY_LIST);
